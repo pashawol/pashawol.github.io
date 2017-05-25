62fbadd4e59c9382c66700910603e92dda9f227b
pashawol <wol1414@gmail.com>
2017-05-25 10:51:25 +0300

    1

---
+++ 0004Avtorazborka/js/common.js
@@ -0,0 +1,15 @@
+$(function() { 
+$('.slider').slick({
+	  infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  dots: false,
+	  speed: 600,
+	   
+	  infinite: false,
+	   prevArrow: ' <div class="l"> </div>',
+  nextArrow: '   <div class="r"> </div> ',
+	});
+
+
+});
