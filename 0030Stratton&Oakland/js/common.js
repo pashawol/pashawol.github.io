00f20c910a5c4516b0e5e65e84b512c9832364d2
pashawol <wol1414@gmail.com>
2017-05-26 21:11:59 +0300

    0030Stratton&Oakland

---
+++ 0030Stratton&Oakland/js/common.js
@@ -0,0 +1,34 @@
+$(function() {
+
+	$('.main').fullpage({
+		// showActiveTooltip: true,
+		// slidesNavigation: true,
+		// responsiveWidth: 1,
+		// responsiveHeight: 1,
+		// responsiveSlides: true, 
+		// // scrollBar: true,
+		// loopBottom: false,
+		// loopTop: false,
+		// loopHorizontal: true,
+		// continuousVertical: true,
+		// continuousHorizontal: true,
+		// scrollHorizontally: true,
+		// interlockedSlides: true,
+		// dragAndMove: true,
+		// offsetSections: true,
+		// // resetSliders: true,
+		// // fadingEffect: true,
+		// // scrollOverflow: true,
+		// scrollOverflowReset: true,
+		// scrollOverflowOptions: null,
+		// touchSensitivity: 15,
+		// // normalScrollElementTouchThreshold: 5,
+		// bigSectionsDestination: null,
+		// controlArrows: true,
+		// verticalCentered: true,
+		// // responsiveSlides: true,
+		scrollOverflow: true,
+		
+	});
+ 
+});
