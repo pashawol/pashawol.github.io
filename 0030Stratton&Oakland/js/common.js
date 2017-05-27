0aa09d9a21e3d6e8ce1323f1e65cc0478d6907ab
pashawol <wol1414@gmail.com>
2017-05-27 10:47:02 +0300

    .block-form

--- 0030Stratton&Oakland/js/common.js
+++ 0030Stratton&Oakland/js/common.js
@@ -27,8 +27,32 @@ $(function() {
 		// controlArrows: true,
 		// verticalCentered: true,
 		// // responsiveSlides: true,
-		scrollOverflow: true,
 		
+navigation:true,
+slidesNavigation:true,
+
+navigationPosition:'right',
+
+slidesNavPosition:'top',
+responsiveWidth:992,
+responsiveHeight: 630,
 	});
- 
+//  var SECTION_DEFAULT_SEL = '.section';
+// var SECTION = 'fp-section';
+// var SECTION_SEL = '.' + SECTION;
+// var SECTION_ACTIVE_SEL = SECTION_SEL + ACTIVE_SEL;
+// var SECTION_FIRST_SEL = SECTION_SEL + ':first';
+// var SECTION_LAST_SEL = SECTION_SEL + ':last';
+// var TABLE_CELL = 'fp-tableCell';
+// var TABLE_CELL_SEL = '.' + TABLE_CELL;
+// var AUTO_HEIGHT = 'fp-auto-height';
+// var AUTO_HEIGHT_SEL = '.fp-auto-height';
+$(".link-wrap a").click(function(){
+	 var elementClick = $(".block-form");
+        var destination = $(elementClick).offset().top;
+        
+            $('html, body').animate({ scrollTop: destination }, 1100);
+        
+        return false; 
+})
 });
