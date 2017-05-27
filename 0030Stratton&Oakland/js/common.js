07aa63239a3a52589e9cae844440a2ce01aea2c9
pashawol <wol1414@gmail.com>
2017-05-27 17:55:57 +0300

    .block-form

--- 0030Stratton&Oakland/js/common.js
+++ 0030Stratton&Oakland/js/common.js
@@ -1,33 +1,7 @@
 $(function() {
 
 	$('.main').fullpage({
-		// showActiveTooltip: true,
-		// slidesNavigation: true,
-		// responsiveWidth: 1,
-		// responsiveHeight: 1,
-		// responsiveSlides: true, 
-		// // scrollBar: true,
-		// loopBottom: false,
-		// loopTop: false,
-		// loopHorizontal: true,
-		// continuousVertical: true,
-		// continuousHorizontal: true,
-		// scrollHorizontally: true,
-		// interlockedSlides: true,
-		// dragAndMove: true,
-		// offsetSections: true,
-		// // resetSliders: true,
-		// // fadingEffect: true,
-		// // scrollOverflow: true,
-		// scrollOverflowReset: true,
-		// scrollOverflowOptions: null,
-		// touchSensitivity: 15,
-		// // normalScrollElementTouchThreshold: 5,
-		// bigSectionsDestination: null,
-		// controlArrows: true,
-		// verticalCentered: true,
-		// // responsiveSlides: true,
-		
+	 
 navigation:true,
 slidesNavigation:true,
 
@@ -35,18 +9,10 @@ navigationPosition:'right',
 
 slidesNavPosition:'top',
 responsiveWidth:767,
-responsiveHeight: 630,
+responsiveHeight: 660,
+normalScrollElements: ".block-form"
 	});
-//  var SECTION_DEFAULT_SEL = '.section';
-// var SECTION = 'fp-section';
-// var SECTION_SEL = '.' + SECTION;
-// var SECTION_ACTIVE_SEL = SECTION_SEL + ACTIVE_SEL;
-// var SECTION_FIRST_SEL = SECTION_SEL + ':first';
-// var SECTION_LAST_SEL = SECTION_SEL + ':last';
-// var TABLE_CELL = 'fp-tableCell';
-// var TABLE_CELL_SEL = '.' + TABLE_CELL;
-// var AUTO_HEIGHT = 'fp-auto-height';
-// var AUTO_HEIGHT_SEL = '.fp-auto-height';
+ 
 $(".link-wrap a").attr("href", ".block-form")
 $(".link-wrap a").click(function(){
 	 var elementClick = $(this).attr("href");
@@ -63,5 +29,11 @@ $(window).on('load', function() {
 
  
 });
-
+$(".block ").each(function(){
+	var th =$(this);
+	th.find(".item").hover(function(){
+		th.find(".item").toggleClass("fade");
+		$(this).toggleClass("fadeIn");
+	})
+})
 });
