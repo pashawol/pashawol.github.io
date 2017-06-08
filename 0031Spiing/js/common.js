139d4701ce76c486cb01116ae38e679b8e452dd4
pashawol <wol1414@gmail.com>
2017-06-08 19:54:57 +0300

    1212

--- 0031Spiing/js/common.js
+++ 0031Spiing/js/common.js
@@ -4,10 +4,16 @@ $(function() {
 				selectSearch: true,
 			});
 	function heightses() {
-
-   
+		var w = $(window).width();
+   if (w> 768){
    $(".otz__item .text-wrap ").height('auto').equalHeights();
-    
+   $(".preim__item p ").height('auto').equalHeights();
+		}	
+    if (w< 768){
+				$(".otz__item .text-wrap ").height('auto');
+   $(".preim__item p ").height('auto');
+
+			}
   }
 
   $(window).resize(function() {
