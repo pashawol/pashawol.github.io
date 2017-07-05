8e946ba69f944f6512992a65d6a56e0255aee9dc
pashah <pashah@gmail.com>
2017-07-05 23:20:55 +0300

    xczxcxzc

--- 0036Bitсoin/js/common.js
+++ 0036Bitсoin/js/common.js
@@ -55,4 +55,15 @@ $(function() {
 
       ]
     });
+
+
+// листалка по стр
+ $("header .btn").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        
+            $('html, body').animate({ scrollTop: destination }, 1100);
+        
+        return false; 
+    });
 });
