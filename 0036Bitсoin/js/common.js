dd7bd0405ed5ee8e231162611aeb7e4e89e7aee8
pashah <pashah@gmail.com>
2017-07-05 19:40:23 +0300

    asssas

--- 0036Bitсoin/js/common.js
+++ 0036Bitсoin/js/common.js
@@ -1,26 +1,6 @@
 $(function() {
 
-	// // Custom JS
-	// function heightses() {
- //    var w= $(window).width()
- //    if (w>1199)
- //    {
- //      $('.prod .row').slick('unslick');
- //    }
-   
- //    if(w<1199){
- //        slick
-      
- //    }
-    
- //  }
- //  $(window).resize(function() {
- //    heightses();
-   
- //  });
- 
-
- // heightses();
+	 
  // ценник в карточках
  $(".prod__price").each(function(){
   var th = $(this)
@@ -34,14 +14,16 @@ $(function() {
   par.find(".prod__price").text(th.data("bit"));
  })
 
-  $('.prod .row').slick({
+  $('.prod .sliders').slick({
       slidesToShow: 4,
       slidesToScroll: 1,
       dots: false,
       speed: 300,
+      // initialSlide: 6,
       infinite: false,
       loop: false,  
       arrows: true,
+      // rtl: true,
       useTransform: false,
        prevArrow: ' <div class="l"><i class="fa fa-angle-left" aria-hidden="true"> </i></div>',
       nextArrow: '   <div class="r"><i class="fa fa-angle-right" aria-hidden="true"> </i></div> ',
