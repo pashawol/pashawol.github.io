a21f28e750aa8036b1a28ba982cbc31195a93f83
pashah <pashah@gmail.com>
2017-07-11 00:34:13 +0300

    ффуу

--- 0036Bitсoin/js/common.js
+++ 0036Bitсoin/js/common.js
@@ -2,17 +2,18 @@ $(function() {
 
 	 
  // ценник в карточках
- $(".prod__price").each(function(){
+ $(".prod__item .currency__price").each(function(){
   var th = $(this)
-  th.text(th.parents(".prod__item").find(".prod__currency-item.active").data("bit"));
+  th.text(th.parents(".prod__item").find(".currency__item.active").data("bit"));
  })
- $(".prod__currency-item").click(function(){
+ $(".prod__item .currency__item").click(function(){
   var th = $(this);
   var par = th.parents(".prod__item");
-  par.find(".prod__currency-item").removeClass("active");
+  par.find(" .currency__item").removeClass("active");
   th.addClass("active");
-  par.find(".prod__price").text(th.data("bit"));
+  par.find(" .currency__price").text(th.data("bit"));
  })
+
 
   $('.prod .sliders').slick({
       slidesToShow: 4,
@@ -66,4 +67,97 @@ $(function() {
         
         return false; 
     });
+
+ 
+function heightses() {
+    var w = $(window).width();
+    
+    
+    
+    var topH=$(".top_line  ").height();
+  if($(this).scrollTop()>topH){
+                  $('.top_line  ').addClass('fixed');
+                  
+              }
+              else if ($(this).scrollTop()<topH){
+                  $('.top_line  ').removeClass('fixed');
+                 
+              }
+  $(window).scroll(function(){
+              if($(this).scrollTop()>topH){
+                  $('.top_line  ').addClass('fixed');
+               
+              }
+              else if ($(this).scrollTop()<topH){
+                  $('.top_line  ').removeClass('fixed');
+                
+              }
+          });
+      
+  }
+  $(window).resize(function() {
+    heightses();
+  });
+
+  $( window ).on( "load", function() {
+ heightses();
+})
+
+// мобильное меню
+   var toggMnu = $(".toggle-mnu-1").click(function () {
+    
+    $(this).toggleClass("on");
+    // $("body").toggleClass("fixed");
+    $(".hidden-mnu").slideToggle();
+    return false;
+  }); 
+    $('.hidden-mnu ul li a').on('click', function () {
+      $(".toggle-mnu-1").click();
+    });
+    
+
+      $('.prod__btn').each(function(){
+        var th = $(this),
+            th2= th.parents(".prod__item");
+        th.attr("href","#small-dialog")
+        .click(function(){
+            // ценник в модалке
+          $(".modal-form ").each(function(){
+              $(this).find(".currency__price").val(th2.find(".currency__item.active").data("bit")).text(th2.find(".currency__item.active").data("bit"));
+              $(this).find(".currency__item:nth-child(1)").data("bit",th2.find(".currency__item:nth-child(1)").data("bit"))
+              $(this).find(".currency__item:nth-child(2)").data("bit",th2.find(".currency__item:nth-child(2)").data("bit"))
+              $(this).find(".currency__item:nth-child(3)").data("bit",th2.find(".currency__item:nth-child(3)").data("bit"))
+             })
+          
+        })
+
+        th.magnificPopup({
+      type: 'inline',
+
+      fixedContentPos: true,
+      fixedBgPos: true,
+
+      overflowY: 'auto',
+
+      closeBtnInside: true,
+      preloader: false,
+      
+      midClick: true,
+      removalDelay: 300,
+      mainClass: 'my-mfp-zoom-in'
+    }); 
+
+
+      })
+     // ценник в модалке
+ 
+ $(".currency__item").click(function(){
+  var th = $(this);
+  var par = th.parents(".modal-form");
+  par.find(".currency__item").removeClass("active");
+  th.addClass("active");
+  par.find(".currency__price").val(th.data("bit")).text(th.data("bit"));
+ })
+
+
 });
