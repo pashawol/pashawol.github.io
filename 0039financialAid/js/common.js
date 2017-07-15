f248e363a5f1539adb4374707322d4f6719a1529
pashah <pashah@gmail.com>
2017-07-16 02:34:00 +0300

    cbv

---
+++ 0039financialAid/js/common.js
@@ -0,0 +1,105 @@
+$(function() {
+
+	// Custom JS
+	function heightses() {
+
+   
+   // $(".otz__item .text-wrap ").height('auto').equalHeights();
+    
+  }
+
+  $(window).resize(function() {
+    heightses();
+   
+  });
+$( window ).on( "load", function() {
+ heightses();
+
+})
+
+ heightses();
+
+
+
+// листалка по стр
+ $(".top-nav a").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        
+            $('html, body').animate({ scrollTop: destination }, 1100);
+        
+        return false; 
+    });
+
+
+ 
+// мобильное меню
+   var toggMnu = $(".toggle-mnu-1").click(function () {
+    
+    $(this).toggleClass("on");
+    // $("body").toggleClass("fixed");
+    $(".hidden-mnu").slideToggle();
+    return false;
+  }); 
+    $('.hidden-mnu ul li a').on('click', function () {
+      $(".toggle-mnu-1").click();
+    });
+
+    // маска на инпут
+    $("input[type='tel']").inputmask("+7(999)999-99-99").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}");
+     // модалка
+  $(".btn-modal").each(function(){
+    $(this).attr("href", "#small-dialog")
+    // $(this).click(function(){
+    //   var th = $(".modal-form");
+    //   th.find(".order").val($(this).data("order"));
+    //   // th.find("p").text($(this).data("p"));
+    // })
+  })
+
+
+    // модалка
+   $('.btn-modal').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: true,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  }); 
+ 
+ $(".calc__item").each(function(){
+  var th = $(this),
+     symbol = th.data("symbol")
+  th.find( ".calc__range" ).slider({
+      range: "max",
+      min: 0,
+      max: 10000,
+      value: 1,
+      slide: function( event, ui ) {
+       th.find( ".calc__price " ).val( ui.value +" "+ symbol);
+      }
+    });
+    // $( ".filter__distance " ).text( $( ".filter__custom-handle-distance   " ).slider( "value" )+" км" );
+    
+     th.find(".calc__range").draggable();
+});
+ // видео в мод окне
+ $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    // disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+ 
+});
