c5cb538b77a08a0cf2c94e473934188cb1cf3499
pashah <pashah@gmail.com>
2017-07-29 15:25:25 +0300

    dasad

--- Vospennikov/0001SEO-group/public/js/common.js
+++ Vospennikov/0001SEO-group/public/js/common.js
@@ -1,11 +1,46 @@
 $(function() {
 
-	// Custom JS
-	function heightses() {
+ 
+// листалка по стр
+ $(" .top-nav a").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        
+            $('html, body').animate({ scrollTop: destination }, 1100);
+        
+        return false; 
+    });
 
-   
-   $(".otz__item .text-wrap ").height('auto').equalHeights();
+
+
+// мобильное меню
+   var toggMnu = $(".toggle-mnu-1").click(function () {
     
+    $(".toggle-mnu-1").toggleClass("on");
+    // $("body").toggleClass("fixed");
+    $(".hidden-mnu").toggleClass("active");
+    $("body").toggleClass("fixed");
+    return false;
+  }); 
+    $('.hidden-mnu ul li a').on('click', function () {
+      $(".toggle-mnu-1").click();
+    });
+
+
+// Custom JS
+  function heightses() {
+    var w = $(window).width();
+   
+   // $(".otz__item .text-wrap ").height('auto').equalHeights();
+   // $(".news__item h3 ").height('auto').equalHeights();
+   // $(".complex__item ").height('auto').equalHeights();
+    
+    if (w>991){
+       $(".toggle-mnu-1").removeClass("on");
+        // $("body").removeClass("fixed");
+        $(".hidden-mnu").removeClass("active");
+        $("body").removeClass("fixed");
+    }
   }
 
   $(window).resize(function() {
@@ -18,18 +53,6 @@ $( window ).on( "load", function() {
 })
 
  heightses();
-
-
-
-// листалка по стр
- $(" .top-nav a").click(function () {
-        var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top;
-        
-            $('html, body').animate({ scrollTop: destination }, 1100);
-        
-        return false; 
-    });
 
 
   // маска на инпут
