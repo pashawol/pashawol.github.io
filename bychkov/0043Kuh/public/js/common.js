192caad781dc65c14c56384ab8910f3519804f6f
pashah <pashah@gmail.com>
2017-08-19 00:12:43 +0300

    fsffdsfdsf

--- bychkov/0043Kuh/public/js/common.js
+++ bychkov/0043Kuh/public/js/common.js
@@ -14,39 +14,13 @@ $(function() {
     $('.hidden-mnu ul li a').on('click', function () {
       $(".hidden-mnu .toggle-mnu").click();
     });
-
-  function heightses() {
-
-    var w = $(window).width();
-   $(".otz__item .text-wrap ").height('auto').equalHeights();
-    
-
-    // скрывает моб меню 
-    if (w>991){
-       $(".toggle-mnu-1").removeClass("on");
-        // $("body").removeClass("fixed");
-        $(".hidden-mnu").removeClass("active");
-        $("body").removeClass("fixed");
-    }
-  }
-
-  $(window).resize(function() {
-    heightses();
-   
-  });
-$( window ).on( "load", function() {
- heightses();
-
-})
-
- heightses();
-
-
-
+ 
+ 
 // листалка по стр
  $(" .top-nav a, .a-scroll, .wrapp-hidden li a").click(function () {
+        var h = $(".top-nav").height();
         var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top + 40;
+        var destination = $(elementClick).offset().top - h;
         
             $('html, body').animate({ scrollTop: destination }, 1100);
         
@@ -61,10 +35,7 @@ $(' .tabs').on('click', '.tabs__btn:not(.active)', function(e) {
     .addClass('active').siblings().removeClass('active')
     .closest('div.tabs').find('.tabs__content').hide().removeClass('active')
     .eq($(this).index()).fadeIn().addClass('active');
-   
-    $('.tabs__slider').slick('unslick');
-     
-
+    
     return false;
 });
 });
@@ -92,6 +63,7 @@ $(' .tabs-c__caption').on('change', 'input', function(e) {
     fixedContentPos: false
   });
   
+  // стрелки для слайдера
  var arrl = (' <div class="l"><svg   xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 455 455" style="enable-background:new 0 0 455 455;" xml:space="preserve"   sodipodi:docname="arr-left.svg" inkscape:version="0.92.1 r15371"><path d="M 455,455 V 0 H 0 V 455 Z M 275.435,99.411 296.683,120.589 190.12,227.5 296.683,334.411 275.435,355.589 147.763,227.5 Z" id="path2" inkscape:connector-curvature="0" style="fill:#ff5320" /></svg></div>'),
       arrr =(' <div class="r"><svg   xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" style="enable-background:new 0 0 455 455;" viewBox="0 0 455 455" version="1.1"><path id="path2" fill="#ff5320" d="M0,0v455h455V0H0z M179.565,355.589l-21.248-21.178L264.88,227.5L158.317,120.589l21.248-21.178L307.237,227.5  L179.565,355.589z" /></svg></div> '),
       arrl2 = (' <div class="l"><svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" viewBox="0 0 370.814 370.814" style="enable-background:new 0 0 370.814 370.814;" xml:space="preserve" sodipodi:docname="left-arrow-chevron.svg" inkscape:version="0.92.1 r15371"><polygon points="292.92,24.848 268.781,0 77.895,185.401 268.781,370.814 292.92,345.961 127.638,185.401   " fill="#ff5320" id="polygon2" /></svg></div>'),
@@ -138,6 +110,8 @@ $(' .tabs-c__caption').on('change', 'input', function(e) {
     
  
 // слайдеры в отзывах
+
+ function  section_slider3(){
 $(".slider-small").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -148,7 +122,7 @@ $(".slider-small").slick({
     arrows: false,
      arrowa:false,
     dots: true,
-     
+    lazyLoad: 'ondemand', 
     customPaging : function(slider, i) {
         var thumb = $(slider.$slides[i]).data('thumb');
         return '<a style="background-image: url('+thumb+');"><img src="'+thumb+'"></a>';
@@ -156,60 +130,40 @@ $(".slider-small").slick({
 
      
 });
+  }
+    section_slider3();
   
-$(".slider-big").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    speed: 450,
-    infinite: true,
-    loop: true,  
-    swipe: false, 
-    arrows:false,
-    dots: false,
-    draggable: false,
-    fade: true,
-    adaptiveHeight: true,
-   
-});
-  // кнопки переключения слайдов
-  $(".reviews").find('.right-arr').on('click', function(){ 
-      $(".reviews").find(' .slider-big').slick("slickNext"); 
+ 
+ 
+// довести до ума
+ $(".reviews").find('.right-arr').on('click', function(){ 
+      $(' .reviews__slide-big.active').removeClass("active").hide().next().fadeIn().addClass("active");
+      if ($(' .reviews__slide-big.active').is(":last-child")== true){
+           $(' .reviews__slide-big.active').removeClass("active").hide()
+          $(' .reviews__slide-big:first-child').fadeIn().addClass("active");
+      }
+          $('.slider-small').slick('unslick');
+    section_slider3();
   }) 
   $(".reviews").find('.left-arr').on('click', function(){ 
-      $(".reviews").find('.slider-big').slick("slickPrev"); 
+      $('.reviews__slide-big.active').removeClass("active").hide().prev().fadeIn().addClass("active"); 
+       if ($(' .reviews__slide-big.active').is(":first-child")== true){
+           $(' .reviews__slide-big.active').removeClass("active").hide()
+          $(' .reviews__slide-big:last-child').fadeIn().addClass("active");
+      }
+        $('.slider-small').slick('unslick');
+        section_slider3();
   }) 
-
+// довести до ума
   // настройки https://eonasdan.github.io/bootstrap-datetimepicker/
 // настройки часовой зоны http://momentjs.com/
 $(".date").datetimepicker({
   locale: 'ru-ru',
   
   format: 'DD.MM.YYYY',
-  tooltips: {
-    today: 'Go to today',
-    clear: 'Clear selection',
-    close: 'Close the picker',
-    selectMonth: 'Select Month',
+  tooltips: { 
     prevMonth:'Прошлый месяц',
-    nextMonth: 'Следующий месяц',
-    selectYear: 'Select Year',
-    prevYear: 'Previous Year',
-    nextYear: 'Next Year',
-    selectDecade: 'Select Decade',
-    prevDecade: 'Previous Decade',
-    nextDecade: 'Next Decade',
-    prevCentury: 'Previous Century',
-    nextCentury: 'Next Century',
-    incrementHour: 'Добавить час',
-    pickHour: 'Выбрать время',
-    decrementHour:'На час меньше',
-    incrementMinute: 'Добавить минуты',
-    pickMinute: 'Выбрать минуты',
-    decrementMinute:'Уменьшить минуты',
-    incrementSecond: 'Increment Second',
-    pickSecond: 'Pick Second',
-    decrementSecond:'Decrement Second',
+    nextMonth: 'Следующий месяц', 
 }
 
 });
@@ -218,51 +172,18 @@ $(".time").datetimepicker({
    format: 'LT',
    
    tooltips: {
-    today: 'Go to today',
-    clear: 'Clear selection',
-    close: 'Close the picker',
-    selectMonth: 'Select Month',
-    prevMonth:'Прошлый месяц',
-    nextMonth: 'Следующий месяц',
-    selectYear: 'Select Year',
-    prevYear: 'Previous Year',
-    nextYear: 'Next Year',
-    selectDecade: 'Select Decade',
-    prevDecade: 'Previous Decade',
-    nextDecade: 'Next Decade',
-    prevCentury: 'Previous Century',
-    nextCentury: 'Next Century',
+     
     incrementHour: 'Добавить час',
     pickHour: 'Выбрать время',
     decrementHour:'На час меньше',
     incrementMinute: 'Добавить минуты',
     pickMinute: 'Выбрать минуты',
     decrementMinute:'Уменьшить минуты',
-    incrementSecond: 'Increment Second',
-    pickSecond: 'Pick Second',
-    decrementSecond:'Decrement Second',
-
+ 
 }
  
 });
-  // кастомный инпут файл
- // $(".file-upload input[type=file]").change(function(){
- //         var filename = $(this).val().replace(/.*\\/, "");
- //        $(this).parents(".formm-wrap").find(".filename").text(filename);
- //        if ($(".filename").text() != ''){
- //                $(this).parents(".formm-wrap").find(".file-name-wrap span").prepend('<img src="img/error.png" alt="" class="error">');
- //              }
- //        else {
- //           $(this).parents(".formm-wrap").find(".file-name-wrap").remove('.res-i');
- //        }
- //    });
- // $(".formm-wrap ").on("click", ".error", function(){
- //    var th= $(this);
- //    th.find(".file-upload input[type=file]").val("");
- //    th.find(".file-upload .filename").text("");
- // })
-
-
+ 
  // кастомный инпут файл 
 var $_input  = function(){
   $(".formm-wrap").each(function(e){ 
