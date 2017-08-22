2d832a7755aa64ef648a209fcf55628f8c7aacf4
pashah <pashah@gmail.com>
2017-08-22 12:13:16 +0300

    asasa

--- Glav/0002AutoBiz/public/js/common.js
+++ Glav/0002AutoBiz/public/js/common.js
@@ -15,20 +15,16 @@ $(function() {
       $(".hidden-mnu .toggle-mnu").click();
     });
 
-  function heightses() {
-
+  function heightses() { 
     var w = $(window).width();
-   $(".otz__item .text-wrap ").height('auto').equalHeights();
-    
-
+   $(".otz__item .text-wrap ").height('auto').equalHeights(); 
     // скрывает моб меню 
     if (w>991){
        $(".toggle-mnu-1").removeClass("on");
         // $("body").removeClass("fixed");
         $(".hidden-mnu").removeClass("active");
         $("body").removeClass("fixed");
-    }
-  
+    } 
     var topH=$("header ").innerHeight();  
     if($(this).scrollTop()>topH){
                     $('.top-nav  ').addClass('fixed');
@@ -49,12 +45,9 @@ $(function() {
                 }
             });
        // конец добавил 
-  }
-
-
+  } 
   $(window).resize(function() {
-    heightses();
-   
+    heightses(); 
   });
 $( window ).on( "load", function() {
  heightses();
@@ -66,30 +59,161 @@ $( window ).on( "load", function() {
 
 
 // листалка по стр
- $(" .top-nav a").click(function () {
+ $(" .top-nav ul a.scroll-link").click(function () {
         var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top;
+        var destination = $(elementClick).offset().top - $(".top-nav").height();
         
             $('html, body').animate({ scrollTop: destination }, 1100);
         
         return false; 
     });
 // табы
+
+$(function() { 
+$(' .tabs1__caption   .tabs1__btn:first-child  ').addClass("active")
+
+ $('.tabs1__content:first-child ').addClass("active");
+$(' .tabs1').on('click , mouseover', '.tabs1__btn:not(.active)', function(e) {
+
+  $(this)
+    .addClass('active').addClass('current').siblings().removeClass('active')
+    .closest('div.tabs1').find('div.tabs1__content').hide().removeClass('active')
+    .eq($(this).index()).fadeIn(150).addClass('active'); 
+});
+});
+ 
+
 $(function() {
-// $(' .tabs__caption   .tab-btn:first-child  ').addClass("active")
- // $('.tabs__content:first-child ').addClass("active");
-$(' .tabs__caption').on('click', '.tab-btn:not(.active)', function(e) {
+$(' .tabs__caption   .tabs__btn:first-child  ').addClass("active")
+ $('.tabs__content:first-child ').addClass("active");
+$(' .tabs').on('click', '.tabs__btn:not(.active)', function(e) {
 
   $(this)
     .addClass('active').addClass('current').siblings().removeClass('active')
     .closest('div.tabs').find('div.tabs__content').hide().removeClass('active')
-    .eq($(this).index()).fadeIn().addClass('active');
+    .eq($(this).index()).fadeIn(150).addClass('active');
    
     // $('.slider-small, .slider-big').slick('unslick');
     //  section_slider();
     return false;
 });
 });
+
+
+
+// для svg
+ //Replace all SVG images with inline SVG
+  $('img.img-svg').each(function(){
+    var $img = $(this);
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+      }, 'xml');
+
+  });
+
+
+  var arrl = (' <div class="l"></div>'),
+      arrr =(' <div class="r"></div> ');
+      
+
+
+   $('.sliderfor5').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false,
+        speed: 450,
+        infinite: true,
+        loop: true,  
+        arrows: true,  
+        prevArrow: arrl,
+        nextArrow: arrr,
+        responsive: [
+      
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        
+        }
+      }
+    ]
+        }); 
+
+   
+   $('.sliderfor3').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        speed: 450,
+        infinite: true,
+        loop: true,  
+        arrows: true,  
+        prevArrow: arrl,
+        nextArrow: arrr,
+        responsive: [
+      
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+        
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+        
+        }
+      }
+    ]
+        }); 
+
+   $(".time").datetimepicker({
+  locale: 'ru-ru', 
+   format: 'LT',
+   
+   tooltips: {
+     
+    incrementHour: 'Добавить час',
+    pickHour: 'Выбрать время',
+    decrementHour:'На час меньше',
+    incrementMinute: 'Добавить минуты',
+    pickMinute: 'Выбрать минуты',
+    decrementMinute:'Уменьшить минуты',
+ 
+}
+ 
+});
+ 
   // маска на инпут
     $("input[type='tel']").inputmask("+7(999)999-99-99").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}");
 });
--- Sidorenko/public/js/common.js
+++ Sidorenko/public/js/common.js
@@ -1,5 +1,25 @@
 $(function() {
 
+
+ function heightses() {
+
+    var w = $(window).width();
+     $(".tabs__pablic-item p").height('auto').equalHeights();
+ 
+  }
+
+  $(window).resize(function() {
+    heightses();
+   
+  });
+$( window ).on( "load", function() {
+ heightses();
+
+})
+
+ heightses();
+ 
+ 
  
 // табы
 $(function() {
@@ -12,7 +32,7 @@ $(' .tabs__caption').on('click', '.tabs__btn:not(.active)', function(e) {
     .closest('div.tabs').find('div.tabs__content1').hide().removeClass('active')
     .eq($(this).index()).fadeIn().addClass('active');
    
-   
+   	 heightses();
     return false;
 });
 });
