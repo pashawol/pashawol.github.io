484f1f5ad4b61362d84a537ba6a6df71a40370e9
pashah <pashah@gmail.com>
2017-08-22 20:04:56 +0300

    фыфыф

--- Glav/0002AutoBiz/public/js/common.js
+++ Glav/0002AutoBiz/public/js/common.js
@@ -213,7 +213,22 @@ $(' .tabs').on('click', '.tabs__btn:not(.active)', function(e) {
 }
  
 });
- 
+         // модальное окно
+   $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
   // маска на инпут
     $("input[type='tel']").inputmask("+7(999)999-99-99").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}");
 });
