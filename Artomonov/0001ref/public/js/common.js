f7d83ee3cccd70abe739b77d6e997a41cd4a9cab
pashah <pashah@gmail.com>
2017-10-12 22:18:26 +0300

    asasas

---
+++ Artomonov/0001ref/public/js/common.js
@@ -0,0 +1,101 @@
+ $(document).ready(function() { 
+
+  // Custom JS
+  
+// мобильное меню
+   var toggMnu = $(".toggle-mnu-1").click(function () {
+    
+    $(".toggle-mnu-1").toggleClass("on");
+    // $("body").toggleClass("fixed");
+    $(".hidden-mnu").toggleClass("active");
+    $("body").toggleClass("fixed");
+    return false;
+  }); 
+    $('.hidden-mnu ul li a').on('click', function () {
+      $(".hidden-mnu .toggle-mnu").click();
+    });
+
+  function heightses() {
+
+    var w = $(window).width();
+   // $(".otz__item .text-wrap ").height('auto').equalHeights();
+   //
+    
+
+    // скрывает моб меню 
+    if (w>991){
+       $(".toggle-mnu-1").removeClass("on");
+        // $("body").removeClass("fixed");
+        $(".hidden-mnu").removeClass("active");
+        $("body").removeClass("fixed");
+    }
+    var topH=$("header ").innerHeight();  
+    if($(this).scrollTop()>topH){
+                    $('.top-nav  ').addClass('fixed');
+                    
+                }
+                else if ($(this).scrollTop()<topH){
+                    $('.top-nav  ').removeClass('fixed');
+                   
+                }
+    $(window).scroll(function(){
+                if($(this).scrollTop()>topH){
+                    $('.top-nav  ').addClass('fixed'); 
+                }
+                else if ($(this).scrollTop()<topH){
+                    $('.top-nav  ').removeClass('fixed'); 
+                }
+            });
+       // конец добавил 
+  }
+
+  $(window).resize(function() {
+    heightses();
+   
+  });
+$( window ).on( "load", function() {
+ heightses();
+
+})
+
+ heightses();
+
+   // в статье скрывает текст
+ $(".mob-read-more").readmore({
+    speed: 300,  
+    maxHeight: 100,
+    moreLink: '<a href="" class="btn-more  "><span>далее</span> </a>',
+lessLink: ' '
+});
+
+ $(".mob-read-more-2").readmore({
+    speed: 300,  
+    maxHeight: 345,
+    moreLink: '<a href="" class="btn-more  "><span>далее</span> </a>',
+lessLink: ' '
+});
+
+ 
+       // модальное окно
+   $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: true,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+ 
+    
+ // маска на инпут
+   $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
+
+   $('select').styler();
+});
