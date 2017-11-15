ce351b64027a196336ea8ad7d3a8f856a82d4710
pashah <pashah@gmail.com>
2017-11-15 21:11:40 +0300

    sdsdsdsd

---
+++ MAKAR/0007slider/public/js/common.js
@@ -0,0 +1,19 @@
+ $(document).ready(function() { 
+
+  
+
+  // карусель
+ $('.top-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  speed: 450,
+  infinite: true,
+  loop: true,  
+  arrows: false
+   
+  });
+
+
+    
+});
