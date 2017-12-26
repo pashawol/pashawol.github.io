3f68ccb2b76a434cceb5884a2158780ad34eab38
pashah <pashah@gmail.com>
2017-12-27 01:04:36 +0300

    sdfadsfss

--- other/0007landing/mob/public/js/common.js
+++ other/0007landing/mob/public/js/common.js
@@ -7,29 +7,33 @@
  
    
  $('.slider__block').slick({
- 		slidesToShow: 1,
-    slidesToScroll: 1, 
+    // slidesToShow: 1,
+   //  slidesToScroll: 1, 
     arrows: false, 
-    dots: false,
-  	asNavFor: '.slider-nav'
+    dots: false,  
+    speed: 450, 
+    asNavFor: '.slider-nav',
+    touchThreshold: 5
 });
 $('.slider-nav').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
+  // slidesToShow: 1,
+  // slidesToScroll: 1,
   asNavFor: '.slider__block',
    arrows: false, 
-  dots: true,
+  dots: true,  
+  speed: 450, 
+  touchThreshold: 5
 });
-		  
+      
  // карусель
  $('.s-brands__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: false,  
-    infinite: true,
-    loop: true,  
     arrows: false,  
-     variableWidth: true,
+     variableWidth: true, 
+     speed: 450,
+     touchThreshold: 5
     
     });
 
