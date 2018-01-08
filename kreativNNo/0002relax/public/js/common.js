58a40f31bd6b19b52923daabd5ab7bd72cb75ea1
pashah <pashah@gmail.com>
2018-01-08 20:41:58 +0300

    fgdfgf

---
+++ kreativNNo/0002relax/public/js/common.js
@@ -0,0 +1,282 @@
+ jQuery(document).ready(function($) { 
+
+  // Custom JS
+  
+
+  var url=document.location.href; 
+  $.each($(".top-nav__nav a "),function(){
+ 
+  if(this.href==url){
+    if($(this).hasClass("top-nav__link") == true){
+
+    $(this).addClass('top-nav__link-active');
+    }
+    if($(this).hasClass("footer__link") == true){
+
+    $(this).addClass('footer__link-active');
+    }
+
+  };
+ 
+  });
+// мобильное меню
+   var toggMnu = $(".toggle-mnu-1").click(function () {
+    
+    $(".toggle-mnu-1").toggleClass("on");
+    // $("body").toggleClass("fixed");
+    $(".hidden-mnu").toggleClass("active");
+    $("body").toggleClass("fixed");
+    return false;
+  }); 
+    $('.hidden-mnu ul li a').on('click', function () {
+      $(".hidden-mnu .toggle-mnu").click();
+    });
+    $(document).mouseup(function (e) {
+    var container = $(".hidden-mnu.active");
+    if (container.has(e.target).length === 0){
+       $(".toggle-mnu-1").removeClass("on");
+      // $("body").toggleClass("fixed");
+      $(".hidden-mnu").removeClass("active");
+      $("body").removeClass("fixed");
+        }
+    });
+ 
+  function heightses() {
+
+    var w = $(window).width();
+   // $(".otz__item .text-wrap ").height('auto').equalHeights();
+   //
+    
+
+    // скрывает моб меню 
+    if (w>991){
+       $(".toggle-mnu-1").removeClass("on");
+        // $("body").removeClass("fixed");
+        $(".hidden-mnu").removeClass("active");
+        $("body").removeClass("fixed");
+    }
+    var topH=$("header ").innerHeight();  
+    if($(this).scrollTop()>topH){
+                    $('.top-nav  ').addClass('fixed');
+                    
+                }
+                else if ($(this).scrollTop()<topH){
+                    $('.top-nav  ').removeClass('fixed');
+                   
+                }
+    $(window).scroll(function(){
+                if($(this).scrollTop()>topH){
+                    $('.top-nav  ').addClass('fixed'); 
+                }
+                else if ($(this).scrollTop()<topH){
+                    $('.top-nav  ').removeClass('fixed'); 
+                }
+            });
+       // конец добавил 
+  }
+
+  $(window).resize(function() {
+    heightses();
+   
+  });
+$( window ).on( "load", function() {
+ heightses();
+
+})
+
+ heightses();
+
+
+
+// листалка по стр
+ // $(" .top-nav a").click(function () {
+ //        var elementClick = $(this).attr("href");
+ //        var destination = $(elementClick).offset().top;
+        
+ //            $('html, body').animate({ scrollTop: destination }, 1100);
+        
+ //        return false; 
+ //    });
+
+
+// или
+ 
+// табы  . Теперь данные активного таба остается в storage
+$(function() {
+var tab = ('tabs'); 
+
+   $('.' + tab + '__caption').each(function(i) {
+    var storage = localStorage.getItem('tab' + i);
+    if (storage) {
+      $(this).find('.' + tab + '__btn').removeClass('active').eq(storage).addClass('active')
+       .closest('.' + tab).find('.' + tab + '__content').removeClass('active').eq(storage).addClass('active');
+    }
+  });
+
+$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function(e) { 
+  $(this) 
+      .addClass('active').siblings().removeClass('active')
+      .closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
+      .eq($(this).index()).fadeIn().addClass('active');
+
+    var ulIndex = $('.' + tab + '__caption').index($(this).parents('.' + tab + '__caption'));
+    localStorage.removeItem('tab' + ulIndex);
+    localStorage.setItem('tab' + ulIndex, $(this).index());
+ 
+});
+});
+
+ var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
+
+var   arrl2 = (' <div class="l">'+ icon),
+      arrr2 =(' <div class="r">'+ icon);
+  
+ // карусель
+ $('.header-block__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    speed: 450,
+    infinite: true,
+    loop: true,  
+    arrows: true,  
+     prevArrow: arrl2,
+      nextArrow: arrr2,
+    });
+
+$('.s-brands__slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: false,
+    speed: 450,
+    infinite: true,
+    loop: true,  
+    arrows: true, 
+    prevArrow: arrl2,
+      nextArrow: arrr2,
+       responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 4,
+       
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3, 
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2, 
+      }
+     } ,
+    {
+      breakpoint: 370,
+      settings: {
+        slidesToShow: 1, 
+      }
+
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    });
+
+
+       // модальное окно
+   $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: true,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+ 
+    // галерея
+  $(".gal").magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+      verticalFit: true,
+      // titleSrc: function(item) {
+      //   return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+      // }
+    },
+    gallery: {
+      enabled: true
+    }
+  });
+ 
+ // маска на инпут
+   $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
+
+  
+    //Replace all SVG images with inline SVG
+  $('img.img-svg').each(function(){
+    var $img = $(this);
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+      }, 'xml');
+
+  });
+
+
+  // кастомный инпут файл 
+ 
+  var file = $(".add-file input[type=file]");
+  file.change(function(){
+         var filename = $(this).val().replace(/.*\\/, "");
+         var name = $(".add-file__filename  ");
+       name.text(filename);
+  
+    }); 
+
+
+  
+  $(".pretty-embed__bg").each(function(){ 
+    // загрузка фона видео
+  $(this).css("background-image",'url(http://img.youtube.com/vi/'  + $(this).data("src")+ '/0.jpg)')
+  // включение видео при клике по блоку
+   $(this).click(function(){
+    $(this).removeClass("on").next()
+    .attr("src", 'https://www.youtube.com/embed/' + $(this).data("src")+'?autoplay=1').addClass("on");
+   })
+   })
+});
