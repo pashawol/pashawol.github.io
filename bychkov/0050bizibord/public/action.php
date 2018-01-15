681f4d53d88e92f81fd9b64e560d6189475125c5
pashah <pashah@gmail.com>
2018-01-15 19:16:31 +0300

    выаваыа

---
+++ bychkov/0050bizibord/public/action.php
@@ -0,0 +1,251 @@
+<?php
+ini_set( 'display_errors', 1 );
+require __DIR__ . '/vendor/autoload.php';
+ // $to='trifectahealthnyc@gmail.com';    
+$mail = new PHPMailer(true);
+$mail->IsMail();
+$mail->IsHTML(true);
+$mail->Priority = '1';
+$mail->Encoding = 'base64';
+$mail->CharSet = 'utf-8';
+
+///от кого письмо  
+$mail->setFrom('info@info.com');
+
+ $mail->addAddress('wol1414@gmail.com');
+ // $mail->addAddress('horenkova369@gmail.com');
+// $mail->addAddress('stab@inbox.support');
+
+
+
+//Субъект
+$mail->Subject = 'Заявка с сайта';
+
+$time = date('d.m.Y в H:i');
+$html = '
+
+<table style="width: 100%;">';
+    if (isset($_POST['order'])) {
+        $html .= ' <tr style="background-color: #f8f8f8;">  <td style="padding: 10px; border: #e9e9e9 1px solid;">Вид формы:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['order'] . '</b></td></tr>';
+    }
+
+    if (isset($_POST['user_name'])) {
+        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;">Name:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['user_name'] . '</b></td></tr>';
+    }
+    if (isset($_POST['tel'])) {
+        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Телефон:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['tel'] . '</b></td></tr>';
+    }
+    if (isset($_POST['text'])) {
+        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Вопрос:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['text'] . '</b></td>';
+    }
+
+    if (isset($_POST['comment'])) {
+        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Отзыв:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['comment'] . '</b></td>';
+    }
+
+    
+    // if (isset($_POST['text'])) {
+    //     $html .= ' <li> Текст сообщения: <b>' . $_POST['text'] . '</b></li>';
+    // }
+    // if (isset($_POST['data'])) {
+    //  $html .= ' <li>Дата рождения: <b>' . $_POST['data'] . '</b></li>';
+    // }
+    // if (isset($_POST['mesto'])) {
+    //  $html .= ' <li>Место учебы или работы: <b>' . $_POST['mesto'] . '</b></li>';
+    // }
+    // if (isset($_POST['video'])) {
+    //  $html .= ' <li>Ссылка : <b>' . $_POST['video'] . '</b></li>';
+    // }
+    // if (isset($_POST['acc'])) {
+    //  $html .= ' <li>Ccылка на аккаунт yotube или страницу в ВК : <b>' . $_POST['acc'] . '</b></li>';
+    // }
+     $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;">  Время отправки:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $time . '</b></td>
+      <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> IP:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_SERVER['REMOTE_ADDR'] . '</b></td> 
+</table>
+';
+$mail->Body = $html;
+
+$uploaddir = __DIR__ . '/upload/';
+
+if ($_FILES['file1']['tmp_name']) {    
+    $mail->addAttachment($_FILES['file1']['tmp_name'],$_FILES['file1']['name']);
+}
+
+if ($_FILES['file2']['tmp_name']) {    
+    $mail->addAttachment($_FILES['file2']['tmp_name'],$_FILES['file2']['name']);
+}
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+if (isset($uploadfile))unlink($uploadfile);
+if (isset($uploadfile2))unlink($uploadfile2);
+
+?>
+
+
+<?php
+/*
+// Адрес, куда отправляем письмо
+//$to = 'test@test.ru ';
+$to = 'test@test.ru';
+
+// Получаем данные от пользователя
+// Все данные обязательно нужно проверять на правильность!
+$userEmail = filter_input(INPUT_POST, 'tel', FILTER_VALIDATE_EMAIL);
+//$subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+//$option = filter_input(INPUT_POST, 'option', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+$message = strip_tags(filter_input(INPUT_POST, 'message', FILTER_SANITIZE_MAGIC_QUOTES), '<p><a><b><div>');
+
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+$data = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+
+$mesto = filter_input(INPUT_POST, 'mesto', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+
+$video = filter_input(INPUT_POST, 'video', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+
+$acc = filter_input(INPUT_POST, 'acc', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+
+
+// Проверка, что данные не пустые. 
+// Нас не интересуют анонимки
+if (empty($name)) {
+   die('Введите Ваше имя.');
+// Нас не интересуют послания с пустым сообщением
+} 
+
+//elseif (empty($message)) {
+//    die('Отсутствует сообщение.');
+//} elseif (empty($subject)) {
+//    $subject = '[тема не указана]';
+//}
+
+$the_file = '';
+//Если пользователь выбрал файл для отправки
+if (!empty($_FILES['file1']['tmp_name'])) {
+    // Закачиваем файл
+    $path = $_FILES['file1']['name'];
+    if (copy($_FILES['file1']['tmp_name'], $path)) {
+        $the_file = $path;
+    }
+}
+$the_file2 = '';
+//Если пользователь выбрал файл для отправки
+if (!empty($_FILES['file2']['tmp_name2'])) {
+    // Закачиваем файл
+    $path2 = $_FILES['file2']['name2'];
+    if (copy($_FILES['file2']['tmp_name2'], $path2)) {
+        $the_file2 = $path2;
+    }
+}
+
+// Если есть прикреплённый файл, то заголовки чуть другие.
+// Поэтому, в зависимости от того, отправил ли пользователь файл,
+// выбираем, что делать дальше
+$headers = null;
+
+if (empty($the_file)) {
+    // эта часть кода отвечает за отправку сообщений без вложений
+    // собираем заголовки
+    $headers = array();
+    $headers[] = "MIME-Version: 1.0";
+    
+    $headers[] = "Content-type: text/html; charset=UTF-8";
+    $headers[] = "From: $name <$userEmail>";
+    $headers[] = "Bcc: JJ Chong <bcc@domain2.com>";
+    $headers[] = "Reply-To: Recipient Name <receiver@domain3.com>";
+    $headers[] = "Subject: {'Изображение с сайта'}";
+    $headers[] = "X-Mailer: PHP/" . phpversion();
+    // собираем текст письма
+    $allmsg = "
+   
+       <p><b>Имя:</b> $name</p></br>
+       <p><b>Дата рождения:</b> $data</p></br>
+       <p><b>МЕСТО УЧЕБЫ ИЛИ РАБОТЫ:</b> $mesto</p></br>
+       <p><b>Видео:</b> $video</p></br>
+       <p><b>АККАУНТ:</b> $acc</p></br>
+       ";
+            
+    $allmsg = "<html><head><title>Обратная связь</title><META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\"></head><body>" . $allmsg . "</body></html>";
+    // отправляем
+    if (!mail($to, $subject, $allmsg, implode("\r\n", $headers))) {
+        echo 'Письмо не отправлено - что-то не сработало.';
+    } else {
+        echo 'Отправлено письмо без вложений.';
+    }
+} else {
+    // эта часть кода отвечает за отправку сообщений без вложений
+    // читаем отправляемый файл в строку
+    $fp = fopen($the_file, "r");
+    if (!$the_file) {
+        die("Ошибка отправка письма: Файл $the_file не может быть прочитан.");
+    }
+    $file = fread($fp, filesize($path));
+    fclose($fp);
+    // удаляем временный файл
+    unlink($path);
+
+    $fp2 = fopen($the_file2, "rr");
+        if (!$the_file2) {
+            die("Ошибка отправка письма: Файл $the_file2 не может быть прочитан.");
+        }
+        $file2 = fread($fp2, filesize($path2));
+        fclose($fp2);
+        // удаляем временный файл
+        unlink($path2);
+
+
+
+    // собираем текст письма
+   $allmsg = "
+     
+       <p><b>Имя:</b> $name</p></br>
+       <p><b>Дата рождения:</b> $data</p></br>
+       <p><b>МЕСТО УЧЕБЫ ИЛИ РАБОТЫ:</b> $mesto</p></br>
+       <p><b>Видео:</b> $video</p></br>
+       <p><b>АККАУНТ:</b> $acc</p></br>
+       ";
+    $allmsg = "<html><head><title>Обратная связь</title><META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\"></head><body>" . $allmsg . "</body></html>";
+    // генерируем разделитель
+    $boundary = "--" . md5(uniqid(time()));
+    // собираем заголовки
+    $headers = array();
+    $headers[] = "MIME-Version: 1.0";
+    $headers[] = "From: $name <$userEmail>";
+    $headers[] = "Bcc: JJ Chong <bcc@domain2.com>";
+    $headers[] = "Reply-To: Recipient Name <receiver@domain3.com>";
+    $headers[] = "Subject: {'Изображение с сайта'}";
+    $headers[] = "X-Mailer: PHP/" . phpversion();
+    $headers[] = "Content-Type: multipart/mixed; boundary=\"$boundary\"\n";
+    // собираем текст письма + приложенынй файл
+    $multipart = array();
+    $multipart[] = "--$boundary";
+    $multipart[] = "Content-Type: text/html; charset=UTF-8";
+    $multipart[] = "Content-Transfer-Encoding: Quot-Printed\r\n";
+    $multipart[] = "$allmsg\r\n";
+    $multipart[] = "--$boundary";
+    $multipart[] = "Content-Type: application/octet-stream";
+    $multipart[] = "Content-Transfer-Encoding: base64";
+    $multipart[] = "Content-Disposition: attachment; filename = \"" . $path . "\"\r\n";
+    $multipart[] = "Content-Disposition: attachment; filename = \"" . $path2 . "\"\r\n";
+    $multipart[] = chunk_split(base64_encode($file));
+    $multipart[] = chunk_split(base64_encode($file2));
+    $multipart[] = "--$boundary";
+    // отправляем
+    if (!mail($to, $subject, implode("\r\n", $multipart), implode("\r\n", $headers))) {
+        echo 'Письмо не отправлено - что-то не сработало.';
+    } else {
+        echo 'Отправлено письмо с вложениями.';
+    }
+}
+*/
+?>
+
+
+
+
+
+
---
+++ bychkov/0050bizibord/public/js/common.js
@@ -0,0 +1,78 @@
+ jQuery(document).ready(function($) { 
+
+  // Custom JS
+  
+ // accordion
+  $(".showhide").click(function() {
+
+
+     $(".showhide-all").slideUp().parent().removeClass("active"), $(".showhide").removeClass("active"), 
+     $(this).next("div").filter(function() {
+            return "block" == $(this).css("display") }).slideUp().parent().removeClass("active"),
+             $(this).next("div").filter(function() {
+            return "none" == $(this).css("display") }).slideDown().prev("div").addClass("active").parent().addClass("active")})
+  
+
+ var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
+
+var   arrl2 = (' <div class="l">'+ icon),
+      arrr2 =(' <div class="r">'+ icon);  
+ // карусель
+ $('.s-rew__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    speed: 450,
+    infinite: true,
+    loop: true,  
+    arrows: false, 
+    });
+
+
+       // модальное окно
+   $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: true,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+ 
+   
+ // форма
+$("form").submit(function() { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: 'action.php', //Change
+      data: th.serialize()
+    }).success(function() {
+          $.magnificPopup.close();
+             $.magnificPopup.open({
+        items: {
+          src: '#thanks', // can be a HTML string, jQuery object, or CSS selector
+          type: 'inline'
+        }
+      })
+        // window.location.replace("/thanks.html");
+       setTimeout(function() {
+        // Done Functions
+        th.trigger("reset");
+        // $.magnificPopup.close();
+      }, 4000);
+    });
+    return false;
+  });
+  
+ // маска на инпут
+   $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
+ 
+});
