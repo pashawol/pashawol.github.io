54b2c64c75651f7ea7fcce66f3fdd7198d16f806
pashah <pashah@gmail.com>
2018-02-13 21:15:07 +0300

    sdsadsad

--- other/0010meb/public/js/common.js
+++ other/0010meb/public/js/common.js
@@ -1,24 +1,6 @@
  jQuery(document).ready(function($) { 
 
-  // Custom JS
-  
-
-  var url=document.location.href; 
-  $.each($(".top-nav__nav a "),function(){
  
-  if(this.href==url){
-    if($(this).hasClass("top-nav__link") == true){
-
-    $(this).addClass('top-nav__link-active');
-    }
-    if($(this).hasClass("footer__link") == true){
-
-    $(this).addClass('footer__link-active');
-    }
-
-  };
- 
-  });
       // галерея
   $(".gal").each(function(){
     
@@ -106,47 +88,7 @@ $( window ).on( "load", function() {
 
  heightses();
 
-
-
-// листалка по стр
- // $(" .top-nav a").click(function () {
- //        var elementClick = $(this).attr("href");
- //        var destination = $(elementClick).offset().top;
-        
- //            $('html, body').animate({ scrollTop: destination }, 1100);
-        
- //        return false; 
- //    });
-
-
-// или
-// Cache selectors
-
-// табы  . Теперь данные активного таба остается в storage
-$(function() {
-var tab = ('tabs'); 
-
-   $('.' + tab + '__caption').each(function(i) {
-    var storage = localStorage.getItem('tab' + i);
-    if (storage) {
-      $(this).find('.' + tab + '__btn').removeClass('active').eq(storage).addClass('active')
-       .closest('.' + tab).find('.' + tab + '__content').removeClass('active').eq(storage).addClass('active');
-    }
-  });
-
-$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function(e) { 
-  $(this) 
-      .addClass('active').siblings().removeClass('active')
-      .closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-      .eq($(this).index()).fadeIn().addClass('active');
-
-    var ulIndex = $('.' + tab + '__caption').index($(this).parents('.' + tab + '__caption'));
-    localStorage.removeItem('tab' + ulIndex);
-    localStorage.setItem('tab' + ulIndex, $(this).index());
- 
-});
-});
-
+  
  
 
  var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
@@ -185,7 +127,60 @@ var   arrl2 = (' <div class="l">'+ icon),
       nextArrow: arrr2,
     });
 
- $(  '.header-block__slider' )
+  $('.s-gal__slider-big').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    speed: 450,
+    asNavFor: '.s-gal__slider-small',
+    infinite: true,
+    loop: true,
+      arrows: false,
+
+  });
+ $('.s-gal__slider-small').slick({
+      slidesToShow: 12,
+      slidesToScroll: 1,
+      dots: false,
+      speed: 450,
+      infinite: true,
+      loop: true,
+       arrows: true,
+      prevArrow: arrl2,
+      nextArrow: arrr2,
+      // centerMode: true,
+
+      focusOnSelect: true ,
+      asNavFor:  '.s-gal__slider-big',
+
+      responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 8, 
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 5
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 3 
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+ $(  '.header-block__slider, ' +
+    '.s-gal__slider-big, ' + 
+      '.s-gal__slider-small')
  .on('lazyLoaded', function(event, slick, image, imageSource){
      image.parent().css('background-image', 'url(' + image.attr('src') + ')');
  });
@@ -206,36 +201,30 @@ var   arrl2 = (' <div class="l">'+ icon),
     mainClass: 'my-mfp-zoom-in'
   });
  
-    // галерея
-  $(".gal").magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    closeOnContentClick: false,
-    closeBtnInside: false,
-    mainClass: 'mfp-with-zoom mfp-img-mobile',
-    image: {
-      verticalFit: true,
-      // titleSrc: function(item) {
-      //   return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-      // }
-    },
-    gallery: {
-      enabled: true
-    }
-  });
-
+ 
   $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
+    
     type: 'iframe',
     mainClass: 'mfp-fade',
     removalDelay: 160,
-    preloader: false,
-
+    preloader: false, 
     fixedContentPos: false
   });
  
  // маска на инпут
    $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
 
-   
+  // accordion
+  $(".showhide").click(function() {
+
+
+     $(".showhide-all").slideUp().parent().removeClass("active"), $(".showhide").removeClass("active"), 
+     $(this).next("div").filter(function() {
+            return "block" == $(this).css("display") }).slideUp().parent().removeClass("active"),
+             $(this).next("div").filter(function() {
+            return "none" == $(this).css("display") }).slideDown().prev("div").addClass("active").parent().addClass("active")})
+  
+
+ 
+ 
 });
