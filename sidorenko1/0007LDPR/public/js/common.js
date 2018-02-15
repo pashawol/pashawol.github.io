16ee0a0a72292e7355eccae5dab91ec51994e14d
pashah <pashah@gmail.com>
2018-02-16 00:58:29 +0300

    fgfgdgdfgf

---
+++ sidorenko1/0007LDPR/public/js/common.js
@@ -0,0 +1,128 @@
+ jQuery(document).ready(function($) { 
+
+  // Custom JS
+ jQuery.datetimepicker.setLocale('ru');
+  $('.datetimepicker ').datetimepicker({
+    timepicker:false,
+    formatDate:'Y/m/d',
+     // minDate:'-1900/01/02',//yesterday is minimum date(for today use 0 or -1970/01/01)
+     // maxDate:'+2010/01/02',//tomorrow is maximum date calendar
+  
+  });    // галерея
+  $(".gal").each(function(){
+    
+  $(this).find("a").magnificPopup({
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+      verticalFit: true, 
+    },
+    gallery: {
+      enabled: true
+    }
+  });
+  })
+// мобильное меню
+   var toggMnu = $(".toggle-mnu-1").click(function () {
+    
+    $(".toggle-mnu-1").toggleClass("on");
+    // $("body").toggleClass("fixed");
+    $(".hidden-mnu").toggleClass("active");
+    $("body").toggleClass("fixed");
+    return false;
+  }); 
+    $('.hidden-mnu ul li a').on('click', function () {
+      $(".hidden-mnu .toggle-mnu").click();
+    });
+    $(document).mouseup(function (e) {
+    var container = $(".hidden-mnu.active");
+    if (container.has(e.target).length === 0){
+       $(".toggle-mnu-1").removeClass("on");
+      // $("body").toggleClass("fixed");
+      $(".hidden-mnu").removeClass("active");
+      $("body").removeClass("fixed");
+        }
+    });
+ 
+  function heightses() {
+
+    var w = $(window).width();
+   // $(".otz__item .text-wrap ").height('auto').equalHeights(); 
+    // скрывает моб меню 
+    if (w>991){
+       $(".toggle-mnu-1").removeClass("on");
+        // $("body").removeClass("fixed");
+        $(".hidden-mnu").removeClass("active");
+        $("body").removeClass("fixed");
+    }
+    var topH=$("header ").innerHeight();  
+    if($(this).scrollTop()>topH){
+                    $('.top-nav  ').addClass('fixed');
+                    
+                }
+                else if ($(this).scrollTop()<topH){
+                    $('.top-nav  ').removeClass('fixed');
+                   
+                }
+    $(window).scroll(function(){
+                if($(this).scrollTop()>topH){
+                    $('.top-nav  ').addClass('fixed'); 
+                }
+                else if ($(this).scrollTop()<topH){
+                    $('.top-nav  ').removeClass('fixed'); 
+                }
+            }); 
+  }
+
+  $(window).resize(function() {
+    heightses();
+   
+  }); 
+ heightses(); 
+ 
+       // модальное окно
+   $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: true,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+ 
+
+ // маска на инпут
+   $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
+
+
+
+   $('input.number, .custom-select').styler();
+  // кастомный инпут файл 
+ 
+  var file = $(".add-file input[type=file]");
+  file.change(function(){
+         var filename = $(this).val().replace(/.*\\/, "");
+         var name = $(".add-file__filename  ");
+       name.text(filename);
+  
+    }); 
+
+// включение видео   
+  $(".pretty-embed__bg").each(function(){ 
+   $(this).click(function(){
+    $(this).removeClass("on").next()
+    .attr("src", 'https://www.youtube.com/embed/' + $(this).data("src")+'?autoplay=1').addClass("on");
+   })
+   })
+
+
+});
