9af50fc0f605ee0f2ccaa074affa030b96a82573
pashah <pashah@gmail.com>
2018-02-15 18:31:25 +0300

    xxvcvxv

--- sidorenko1/0007LDPR/public/js/common.js
+++ sidorenko1/0007LDPR/public/js/common.js
@@ -11,10 +11,7 @@
     closeBtnInside: false,
     mainClass: 'mfp-with-zoom mfp-img-mobile',
     image: {
-      verticalFit: true,
-      // titleSrc: function(item) {
-      //   return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-      // }
+      verticalFit: true, 
     },
     gallery: {
       enabled: true
@@ -46,10 +43,7 @@
   function heightses() {
 
     var w = $(window).width();
-   // $(".otz__item .text-wrap ").height('auto').equalHeights();
-   //
-    
-
+   // $(".otz__item .text-wrap ").height('auto').equalHeights(); 
     // скрывает моб меню 
     if (w>991){
        $(".toggle-mnu-1").removeClass("on");
@@ -73,85 +67,15 @@
                 else if ($(this).scrollTop()<topH){
                     $('.top-nav  ').removeClass('fixed'); 
                 }
-            });
-       // конец добавил 
+            }); 
   }
 
   $(window).resize(function() {
     heightses();
    
-  });
-$( window ).on( "load", function() {
- heightses();
-
-})
-
- heightses();
-
-
-
-// листалка по стр
- // $(" .top-nav a").click(function () {
- //        var elementClick = $(this).attr("href");
- //        var destination = $(elementClick).offset().top;
-        
- //            $('html, body').animate({ scrollTop: destination }, 1100);
-        
- //        return false; 
- //    });
-
-
-// или
-// Cache selectors
+  }); 
+ heightses(); 
  
-// табы  . Теперь данные активного таба остается в storage
-$(function() {
-var tab = ('tabs'); 
-
-   $('.' + tab + '__caption').each(function(i) {
-    var storage = localStorage.getItem('tab' + i);
-    if (storage) {
-      $(this).find('.' + tab + '__btn').removeClass('active').eq(storage).addClass('active')
-       .closest('.' + tab).find('.' + tab + '__content').removeClass('active').eq(storage).addClass('active');
-    }
-  });
-
-$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function(e) { 
-  $(this) 
-      .addClass('active').siblings().removeClass('active')
-      .closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-      .eq($(this).index()).fadeIn().addClass('active');
-
-    var ulIndex = $('.' + tab + '__caption').index($(this).parents('.' + tab + '__caption'));
-    localStorage.removeItem('tab' + ulIndex);
-    localStorage.setItem('tab' + ulIndex, $(this).index());
- 
-});
-});
-
- 
-
- var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
-
-var   arrl2 = (' <div class="l">'+ icon),
-      arrr2 =(' <div class="r">'+ icon);  
- // карусель
- $('.s-team__slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: false,
-    speed: 450,
-    infinite: true,
-    loop: true,  
-    arrows: true, 
-    // centerMode: true,
-    // focusOnSelect: true , 
-     // variableWidth: true,
-     prevArrow: arrr2,
-      nextArrow: arrl2,
-    });
-
-
        // модальное окно
    $('.popup-with-move-anim').magnificPopup({
     type: 'inline',
@@ -184,30 +108,13 @@ var   arrl2 = (' <div class="l">'+ icon),
   
     }); 
 
-
- 
-   
+// включение видео   
   $(".pretty-embed__bg").each(function(){ 
-    // загрузка фона видео
-  // $(this).css("background-image",'url(http://img.youtube.com/vi/'  + $(this).data("src")+ '/0.jpg)')
-  // включение видео при клике по блоку
    $(this).click(function(){
     $(this).removeClass("on").next()
     .attr("src", 'https://www.youtube.com/embed/' + $(this).data("src")+'?autoplay=1').addClass("on");
    })
    })
 
-
-  // $(".item-block__prev-video").each(function(){ 
-  //   // загрузка фона видео
-  // // $(this).css("background-image",'url(http://img.youtube.com/vi/'  + $(this).data("src")+ '/0.jpg)')
-  // // включение видео при клике по блоку
-  // var th = $(this);
-  //  th.click(function(){
-  //   th.addClass("on").find("iframe")
-  //   .attr("src", 'https://www.youtube.com/embed/' + th.data("src")+'?autoplay=1');
-  //   console.log('1');
-  //  })
-  //  })
 
 });
