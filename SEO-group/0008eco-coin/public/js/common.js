feac83aee6caea08cf38f6d293a9f35088fbed26
pashah <pashah@gmail.com>
2018-02-18 00:32:55 +0300

    ddasfsdf

--- SEO-group/0008eco-coin/public/js/common.js
+++ SEO-group/0008eco-coin/public/js/common.js
@@ -1,28 +1,7 @@
  jQuery(document).ready(function($) { 
 
   // Custom JS
-  
  
-      // галерея
-  $(".gal").each(function(){
-    
-  $(this).find("a").magnificPopup({
-    type: 'image',
-    closeOnContentClick: false,
-    closeBtnInside: false,
-    mainClass: 'mfp-with-zoom mfp-img-mobile',
-    image: {
-      verticalFit: true,
-      // titleSrc: function(item) {
-      //   return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-      // }
-    },
-    gallery: {
-      enabled: true
-    }
-  });
-  })
-// мобильное меню
    var toggMnu = $(".toggle-mnu-1").click(function () {
     
     $(".toggle-mnu-1").toggleClass("on");
@@ -92,25 +71,16 @@ $( window ).on( "load", function() {
 
 
 // листалка по стр
- // $(" .top-nav a").click(function () {
- //        var elementClick = $(this).attr("href");
- //        var destination = $(elementClick).offset().top;
+ $(" .top-nav a").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
         
- //            $('html, body').animate({ scrollTop: destination }, 1100);
+            $('html, body').animate({ scrollTop: destination }, 1100);
         
- //        return false; 
- //    });
+        return false; 
+    });
 
-
-// или
-
-
- var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
-
-var   arrl2 = (' <div class="l">'+ icon),
-      arrr2 =(' <div class="r">'+ icon);  
- // карусель
- 
+  
 
 // карусель
  $('.header-block__slider').slick({
@@ -121,33 +91,14 @@ var   arrl2 = (' <div class="l">'+ icon),
     infinite: true,
     loop: true,  
     arrows: false, 
-    // centerMode: true,
-    // focusOnSelect: true , 
-     // variableWidth: true,
-     prevArrow: arrl2,
-      nextArrow: arrr2,
+   
     });
  
   $(  '.header-block__slider  ')
  .on('lazyLoaded', function(event, slick, image, imageSource){
      image.parent().css('background-image', 'url(' + image.attr('src') + ')');
  });
-       // модальное окно
-   $('.popup-with-move-anim').magnificPopup({
-    type: 'inline',
 
-    fixedContentPos: true,
-    fixedBgPos: true,
-
-    overflowY: 'auto',
-
-    closeBtnInside: true,
-    preloader: false,
-    
-    midClick: true,
-    removalDelay: 300,
-    mainClass: 'my-mfp-zoom-in'
-  });
  
    
  // маска на инпут
