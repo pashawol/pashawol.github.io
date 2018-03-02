53dc6fd2b7f810e1e771c3bf5a1ff3312e520dd2
pashah <pashah@gmail.com>
2018-03-03 01:53:26 +0300

    tertert

--- Glav/0010posiflora/public/js/common.js
+++ Glav/0010posiflora/public/js/common.js
@@ -1,5 +1,5 @@
  $(document).ready(function(){
-    $(".phonenum").mask("+7 (999) 999-99-99");
+ 
   $(".fancy").fancybox({
     width : 650,
     height  : 380,
@@ -276,10 +276,6 @@ $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', functio
 });
 });
 
- 
-
- var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
- 
    
  // маска на инпут
    $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
@@ -293,7 +289,16 @@ $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', functio
             return "block" == $(this).css("display") }).slideUp().parent().removeClass("active"),
              $(this).next("div").filter(function() {
             return "none" == $(this).css("display") }).slideDown().prev("div").addClass("active").parent().addClass("active")})
-  
+  $(".gal__link").fancybox({
+      openEffect  : 'elastic',
+      closeEffect : 'elastic',
+
+      helpers : {
+        title : {
+          type : 'inside'
+        }
+      }
+    });
 
 });
 
