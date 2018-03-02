8948374df123d31f98d1ded9745b53469b7c12eb
pashah <pashah@gmail.com>
2018-03-03 01:04:31 +0300

    ohouhnoho

---
+++ Glav/0011sturgus/public/js/common.js
@@ -0,0 +1,26 @@
+ jQuery(document).ready(function($) { 
+
+  // Custom JS
+   
+// мобильное меню
+   var toggMnu = $(".toggle-mnu-1").click(function () {
+    
+    $(".toggle-mnu-1").toggleClass("on");
+    // $("body").toggleClass("fixed");
+    $(".hidden-mnu").toggleClass("active");
+    $("body").toggleClass("fixed");
+    return false;
+  }); 
+   
+    $(document).mouseup(function (e) {
+    var container = $(".hidden-mnu.active");
+    if (container.has(e.target).length === 0){
+       $(".toggle-mnu-1").removeClass("on");
+      // $("body").toggleClass("fixed");
+      $(".hidden-mnu").removeClass("active");
+      $("body").removeClass("fixed");
+        }
+    });
+ 
+  
+});
