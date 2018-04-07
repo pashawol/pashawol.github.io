3c5c77a5d60e4dd59ec886a970b265f3f6a99468
pashah <pashah@gmail.com>
2018-04-08 02:13:45 +0300

    фыфыфы

---
+++ Pavlenko/0002barber/public/js/common.js
@@ -0,0 +1,155 @@
+ jQuery(document).ready(function($) { 
+
+  // для свг
+  svg4everybody({});
+  // Custom JS
+  
+ 
+      // галерея
+  $(".gal").each(function(){
+    
+  $(this).find("a.img-bg").magnificPopup({
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+      verticalFit: true,
+      // titleSrc: function(item) {
+      //   return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+      // }
+    },
+    gallery: {
+      enabled: true
+    }
+  });
+  })
+
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+// мобильное меню
+   var toggMnu = $(".toggle-mnu-1").click(function () {
+    
+    $(".toggle-mnu-1").toggleClass("on");
+    // $("body").toggleClass("fixed");
+    $(".hidden-mnu").toggleClass("active");
+    $("body").toggleClass("fixed");
+    return false;
+  }); 
+    $('.hidden-mnu ul li a').on('click', function () {
+      $(".hidden-mnu .toggle-mnu").click();
+    });
+    $(document).mouseup(function (e) {
+    var container = $(".hidden-mnu.active");
+    if (container.has(e.target).length === 0){
+       $(".toggle-mnu-1").removeClass("on");
+      // $("body").toggleClass("fixed");
+      $(".hidden-mnu").removeClass("active");
+      $("body").removeClass("fixed");
+        }
+    });
+ 
+  function heightses() {
+
+    var w = $(window).width();
+   // $(".otz__item .text-wrap ").height('auto').equalHeights();
+   //
+    
+
+    // скрывает моб меню 
+    if (w>991){
+       $(".toggle-mnu-1").removeClass("on");
+        // $("body").removeClass("fixed");
+        $(".hidden-mnu").removeClass("active");
+        $("body").removeClass("fixed");
+    }
+    var topH=$("header ").innerHeight();  
+    if($(this).scrollTop()>topH){
+                    $('.top-nav  ').addClass('fixed');
+                    
+                }
+                else if ($(this).scrollTop()<topH){
+                    $('.top-nav  ').removeClass('fixed');
+                   
+                }
+    $(window).scroll(function(){
+                if($(this).scrollTop()>topH){
+                    $('.top-nav  ').addClass('fixed'); 
+                }
+                else if ($(this).scrollTop()<topH){
+                    $('.top-nav  ').removeClass('fixed'); 
+                }
+            });
+       // конец добавил 
+  }
+
+  $(window).resize(function() {
+    heightses();
+   
+  });
+$( window ).on( "load", function() {
+ heightses();
+
+})
+
+ heightses();
+
+
+
+// листалка по стр
+ $(" .top-nav a, .scroll-link").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        
+            $('html, body').animate({ scrollTop: destination }, 1100);
+        
+        return false; 
+    });
+
+
+   
+  
+
+       // модальное окно
+   $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: true,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+ 
+   
+ // маска на инпут
+   $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
+
+   
+   
+var wowAnim = $(".animate-block," +
+                ".s-pick__col," + 
+                ".s-condition__col");
+wowAnim.each(function(i){
+ 
+wowAnim.eq(i).attr("data-wow-delay", i*.1*2 + "s"); 
+// $(this).addClass("wow bounceInUp");
+ 
+   var wow = new WOW({ mobile: false });
+        wow.init();
+
+});
+});
