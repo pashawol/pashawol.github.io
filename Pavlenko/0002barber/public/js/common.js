71eafb3c7dfd00b36c692d03fe862b57706f942b
pashah <pashah@gmail.com>
2018-04-08 11:22:44 +0300

    sadsdas

--- Pavlenko/0002barber/public/js/common.js
+++ Pavlenko/0002barber/public/js/common.js
@@ -34,74 +34,7 @@
 
     fixedContentPos: false
   });
-// мобильное меню
-   var toggMnu = $(".toggle-mnu-1").click(function () {
-    
-    $(".toggle-mnu-1").toggleClass("on");
-    // $("body").toggleClass("fixed");
-    $(".hidden-mnu").toggleClass("active");
-    $("body").toggleClass("fixed");
-    return false;
-  }); 
-    $('.hidden-mnu ul li a').on('click', function () {
-      $(".hidden-mnu .toggle-mnu").click();
-    });
-    $(document).mouseup(function (e) {
-    var container = $(".hidden-mnu.active");
-    if (container.has(e.target).length === 0){
-       $(".toggle-mnu-1").removeClass("on");
-      // $("body").toggleClass("fixed");
-      $(".hidden-mnu").removeClass("active");
-      $("body").removeClass("fixed");
-        }
-    });
  
-  function heightses() {
-
-    var w = $(window).width();
-   // $(".otz__item .text-wrap ").height('auto').equalHeights();
-   //
-    
-
-    // скрывает моб меню 
-    if (w>991){
-       $(".toggle-mnu-1").removeClass("on");
-        // $("body").removeClass("fixed");
-        $(".hidden-mnu").removeClass("active");
-        $("body").removeClass("fixed");
-    }
-    var topH=$("header ").innerHeight();  
-    if($(this).scrollTop()>topH){
-                    $('.top-nav  ').addClass('fixed');
-                    
-                }
-                else if ($(this).scrollTop()<topH){
-                    $('.top-nav  ').removeClass('fixed');
-                   
-                }
-    $(window).scroll(function(){
-                if($(this).scrollTop()>topH){
-                    $('.top-nav  ').addClass('fixed'); 
-                }
-                else if ($(this).scrollTop()<topH){
-                    $('.top-nav  ').removeClass('fixed'); 
-                }
-            });
-       // конец добавил 
-  }
-
-  $(window).resize(function() {
-    heightses();
-   
-  });
-$( window ).on( "load", function() {
- heightses();
-
-})
-
- heightses();
-
-
 
 // листалка по стр
  $(" .top-nav a, .scroll-link").click(function () {
@@ -134,11 +67,43 @@ $( window ).on( "load", function() {
     mainClass: 'my-mfp-zoom-in'
   });
  
-   
- // маска на инпут
-   $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
 
-   
+var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
+
+var   arrl2 = (' <div class="l">'+ icon),
+      arrr2 =(' <div class="r">'+ icon);  
+  
+$('.slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4, 
+  prevArrow: arrl2,
+  nextArrow: arrr2,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3, 
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2 
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1, 
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});   
    
 var wowAnim = $(".animate-block," +
                 ".s-pick__col," + 
@@ -152,4 +117,15 @@ wowAnim.eq(i).attr("data-wow-delay", i*.1*2 + "s");
         wow.init();
 
 });
+
+// паралакс
+ 
+ var scrollorama = $.scrollorama({
+        blocks:'.scrollblock'
+      
+    });
+ 
+ scrollorama
+  .animate('.abs-img',{delay: -1200, duration: 1200, property:'top', start:50, end:-100 })
+  .animate('.abs-img2',{delay: -100, duration: 1200, property:'top', start:50, end:-100 })
 });
