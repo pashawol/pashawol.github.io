48b77a910dedba471716e7900611793e19d181bc
pashah <pashah@gmail.com>
2018-04-10 22:07:51 +0300

    zczxc

--- other/0011kosmodel/public/js/common.js
+++ other/0011kosmodel/public/js/common.js
@@ -139,27 +139,6 @@ $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', functio
 });
 
  
-
- var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
-
-var   arrl2 = (' <div class="l">'+ icon),
-      arrr2 =(' <div class="r">'+ icon);  
- // карусель
- // $('.slider-js').slick({
- //    slidesToShow: 3,
- //    slidesToScroll: 1,
- //    dots: false,
- //    speed: 450,
- //    infinite: true,
- //    loop: true,  
- //    arrows: true, 
- //    // centerMode: true,
- //    // focusOnSelect: true , 
- //     // variableWidth: true,
- //     prevArrow: arrr2,
- //      nextArrow: arrl2,
- //    });
-
 // слайдер в шапке
  var swiper = new Swiper('.header-block__slider', {
       navigation: {
@@ -204,76 +183,17 @@ var   arrl2 = (' <div class="l">'+ icon),
     .next().attr("id", 'modal-filter' + i);
  })
  // маска на инпут
-   $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
+   // $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
 
   
-    //Replace all SVG images with inline SVG
-  $('img.img-svg').each(function(){
-    var $img = $(this);
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
+   
 
-    $.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = $(data).find('svg');
-
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-          $svg = $svg.attr('class', imgClass+' replaced-svg');
-        }
-
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
-
-        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-        }
-
-        // Replace image with new SVG
-        $img.replaceWith($svg);
-
-      }, 'xml');
-
-  });
-
+  // кастомный селект
 $('.custom-select-js').each(function(){
 
   $(this).select2();
 })
   // кастомный инпут файл 
  
-  var file = $(".add-file input[type=file]");
-  file.change(function(){
-         var filename = $(this).val().replace(/.*\\/, "");
-         var name = $(".add-file__filename  ");
-       name.text(filename);
-  
-    }); 
-
-
-  
-  $(".pretty-embed__bg").each(function(){ 
-    // загрузка фона видео
-  $(this).css("background-image",'url(http://img.youtube.com/vi/'  + $(this).data("src")+ '/0.jpg)')
-  // включение видео при клике по блоку
-   $(this).click(function(){
-    $(this).removeClass("on").next()
-    .attr("src", 'https://www.youtube.com/embed/' + $(this).data("src")+'?autoplay=1').addClass("on");
-   })
-   })
-
-   
-var wowAnim = $(".s-dop__col," +
-                ".s-pick__col," + 
-                ".s-condition__col");
-wowAnim.each(function(i){
  
-wowAnim.eq(i).attr("data-wow-delay", i*.1*2 + "s"); 
-// $(this).addClass("wow bounceInUp");
- 
-   var wow = new WOW({ mobile: false });
-        wow.init();
-
-});
 });
