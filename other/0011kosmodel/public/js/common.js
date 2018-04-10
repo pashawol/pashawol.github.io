e878eefc2c07389c8a0d84528fffa32103ef593c
pashah <pashah@gmail.com>
2018-04-10 21:16:55 +0300

    фыфыф

--- other/0011kosmodel/public/js/common.js
+++ other/0011kosmodel/public/js/common.js
@@ -112,14 +112,14 @@ $( window ).on( "load", function() {
 
 
 // листалка по стр
- // $(" .top-nav a").click(function () {
- //        var elementClick = $(this).attr("href");
- //        var destination = $(elementClick).offset().top;
+ $(" .scroll-link").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
         
- //            $('html, body').animate({ scrollTop: destination }, 1100);
+            $('html, body').animate({ scrollTop: destination }, 1100);
         
- //        return false; 
- //    });
+        return false; 
+    });
 
 
 // или
@@ -237,7 +237,10 @@ var   arrl2 = (' <div class="l">'+ icon),
 
   });
 
+$('.custom-select-js').each(function(){
 
+  $(this).select2();
+})
   // кастомный инпут файл 
  
   var file = $(".add-file input[type=file]");
