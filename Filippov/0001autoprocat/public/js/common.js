f9e6dc870ac2a35df583f5b3684fd615b43edd80
pashah <pashah@gmail.com>
2018-04-26 21:11:33 +0300

    asasasas

--- Filippov/0001autoprocat/public/js/common.js
+++ Filippov/0001autoprocat/public/js/common.js
@@ -210,10 +210,27 @@ function heightses() {
    })
    })
 
- $(".input-range").ionRangeSlider({
-  type: "double",
-   min: 0,
-    max: 10000,
- });
+ $(".input-range").each(function(){
+  let minV = $(this).data("min"),
+      maxV = $(this).data("max") ;
 
+     $(this).ionRangeSlider({
+      type: "double",
+       min: minV,
+        max: maxV,
+     });
+
+ })
+ $(".input-range-single").each(function(){
+  let minV = $(this).data("min"),
+      maxV = $(this).data("max") ;
+
+     $(this).ionRangeSlider({
+      type: "single",
+       min: minV,
+        max: maxV,
+     });
+
+ })
+ 
 });
