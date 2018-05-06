0010dc28e1aae04e64cfdbce3f85785a84dc6a5e
pashah <pashah@gmail.com>
2018-05-06 11:03:09 +0300

    asas

--- bychkov/0053tapaz/public/js/common.js
+++ bychkov/0053tapaz/public/js/common.js
@@ -2,7 +2,7 @@
 
   // Custom JS
   
- 
+  svg4everybody({});
   function heightses() {
 
     var w = $(window).width();
@@ -80,6 +80,40 @@
                 
         //   }
     });
+
+
+       //Replace all SVG images with inline SVG
+  $('img.img-svg').each(function(){
+    var $img = $(this);
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+          $('[fill]').removeAttr('fill');
+                    $('[stroke]').removeAttr('stroke');
+                    $('[style]').removeAttr('style');
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+      }, 'xml');
+
+  });
+
 
    
  // маска на инпут
