fd11059044a39a6d9ffd55ac00f664a38f93811b
pashah <pashah@gmail.com>
2018-05-23 14:55:32 +0300

    asasa

--- Zhook/0005korsakov/public/js/common.js
+++ Zhook/0005korsakov/public/js/common.js
@@ -45,22 +45,10 @@
 
     $(".toggle-mnu-1").toggleClass("on");
     // $("body").toggleClass("fixed");
-    $(".hidden-mnu").toggleClass("active");
+    $(".hidden-mnu").slideToggle();
     $("body, html").toggleClass("fixed");
     return false;
   });
-    $('.hidden-mnu ul li a').on('click', function () {
-      $(".hidden-mnu .toggle-mnu").click();
-    });
-    $(document).mouseup(function (e) {
-    var container = $(".hidden-mnu.active");
-    if (container.has(e.target).length === 0){
-       $(".toggle-mnu-1").removeClass("on");
-      // $("body").toggleClass("fixed");
-      $(".hidden-mnu").removeClass("active");
-      $("body, html").removeClass("fixed");
-        }
-    });
 
   function heightses() {
 
@@ -147,10 +135,6 @@ $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', functio
 
 
 
- var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
-
-var   arrl2 = (' <div class="l">'+ icon),
-      arrr2 =(' <div class="r">'+ icon);
  // карусель
  // $('.s-team__slider').slick({
  //    slidesToShow: 3,
@@ -168,6 +152,18 @@ var   arrl2 = (' <div class="l">'+ icon),
  //    });
 
 // слайдер цвета в карточке
+ var swiper1 = new Swiper('.slider-js', {
+      // slidesPerView: 5,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+    });
+
  var swiper4 = new Swiper('.color-slider', {
       // slidesPerView: 5,
       slidesPerView: 'auto',
@@ -187,6 +183,7 @@ var   arrl2 = (' <div class="l">'+ icon),
       },
 
     });
+
        // модальное окно
    $('.popup-with-move-anim').magnificPopup({
     type: 'inline',
