ad4644cec8fedd8c6640eb353ffaf335baa0b36e
pashah <pashah@gmail.com>
2018-06-22 23:27:26 +0300

    asas

--- ROONIX/0001target/public/js/common.js
+++ ROONIX/0001target/public/js/common.js
@@ -26,22 +26,6 @@ vidFade();
 });
 }
 
-  var url=document.location.href;
-  $.each($(".top-nav__nav a "),function(){
-
-  if(this.href==url){
-    if($(this).hasClass("top-nav__link") == true){
-
-    $(this).addClass('top-nav__link-active');
-    }
-    if($(this).hasClass("footer__link") == true){
-
-    $(this).addClass('footer__link-active');
-    }
-
-  };
-
-  });
       // галерея
   $(".gal").each(function(){
 
@@ -103,6 +87,7 @@ vidFade();
                 else {
                     $('.top-nav  ').removeClass('fixed');
                 }
+
             });
        // конец добавил
   }
@@ -111,14 +96,19 @@ vidFade();
     heightses();
 
   });
-$( window ).on( "load", function() {
- heightses();
 
-})
 
  heightses();
 
+ $(window).scroll(function(){
+		 if($(this).scrollTop()>250){
+				 $('.btn-top--js ').addClass('active');
+		 }
+		 else {
+				 $('.btn-top--js ').removeClass('active');
+		 }
 
+ });
  // листалка по стр
   $(" .header-block__scroll--js").click(function () {
          var elementClick = $(".s-about");
@@ -128,42 +118,17 @@ $( window ).on( "load", function() {
 
          return false;
      });
+ // листалка по стр
+  $(" .btn-top--js").click(function () {
+         var elementClick = $("body");
+         var destination = $(elementClick).offset().top;
 
-// листалка по стр
- // $(" .top-nav a").click(function () {
- //        var elementClick = $(this).attr("href");
- //        var destination = $(elementClick).offset().top;
+             $('html, body').animate({ scrollTop: destination }, 1100);
 
- //            $('html, body').animate({ scrollTop: destination }, 1100);
-
- //        return false;
- //    });
+         return false;
+     });
 
 
-// табы  . Теперь данные активного таба остается в storage
-$(function() {
-var tab = ('tabs');
-
-   $('.' + tab + '__caption').each(function(i) {
-    var storage = localStorage.getItem('tab' + i);
-    if (storage) {
-      $(this).find('.' + tab + '__btn').removeClass('active').eq(storage).addClass('active')
-       .closest('.' + tab).find('.' + tab + '__content').removeClass('active').eq(storage).addClass('active');
-    }
-  });
-
-$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function(e) {
-  $(this)
-      .addClass('active').siblings().removeClass('active')
-      .closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-      .eq($(this).index()).fadeIn().addClass('active');
-
-    var ulIndex = $('.' + tab + '__caption').index($(this).parents('.' + tab + '__caption'));
-    localStorage.removeItem('tab' + ulIndex);
-    localStorage.setItem('tab' + ulIndex, $(this).index());
-
-});
-});
 
 // cлайдер для каталога
 // слайдер в серт
@@ -206,6 +171,15 @@ $(".carusel-js-wrap").each(function(){
 	});
  })
 
+
+	var swiper3 = new Swiper('.filter-carusel-js', {
+
+		slidesPerView: 'auto',
+		spaceBetween: 30,
+		speed: 900,
+		clickable: true,
+	});
+
  var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
 
 var   arrl2 = (' <div class="l">'+ icon),
@@ -245,53 +219,15 @@ var   arrl2 = (' <div class="l">'+ icon),
     mainClass: 'my-mfp-zoom-in'
   });
 
+	 if ($("div").is(".container-mix")){
+		var containerEl = document.querySelector('.container-mix');
+
+	  var mixer = mixitup(containerEl);
+	}
 
 
- // маска на инпут
-   $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
-
-
-
-  // кастомный инпут файл
-
-  var file = $(".add-file input[type=file]");
-  file.change(function(){
-         var filename = $(this).val().replace(/.*\\/, "");
-         var name = $(".add-file__filename  ");
-       name.text(filename);
-
-    });
-  // или
-   // $(".dropzone").dropzone({
-   //  url: "/file/post",
-   //  addRemoveLinks: true,
-   //      acceptedFiles: 'image/*',
-   //      uploadMultiple: true,
-   //   });
-
-
-  $(".pretty-embed__bg").each(function(){
-    // загрузка фона видео
-  $(this).css("background-image",'url(http://img.youtube.com/vi/'  + $(this).data("src")+ '/0.jpg)')
-  // включение видео при клике по блоку
-   $(this).click(function(){
-    $(this).removeClass("on").next()
-    .attr("src", 'https://www.youtube.com/embed/' + $(this).data("src")+'?autoplay=1').addClass("on");
-   })
-   })
-
-
-// var wowAnim = $(".s-dop__col," +
-//                 ".s-pick__col," +
-//                 ".s-condition__col");
-// wowAnim.each(function(i){
-
-// wowAnim.eq(i).attr("data-wow-delay", i*.1*2 + "s");
-
-//    var wow = new WOW({ mobile: false });
-//         wow.init();
-
-// });
-
-
+	$(".filter-drop--js").click(function(){
+		$(this).toggleClass("active");
+		$(".child--js").slideToggle();
+	})
 });
