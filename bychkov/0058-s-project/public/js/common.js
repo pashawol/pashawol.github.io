16c7499d34dcd7104e1efa4100a944aaf79e8d23
Pavel <wol1414@gmail.com>
2018-08-19 23:54:35 +0300

    cc czcxcx

--- bychkov/0058-s-project/public/js/common.js
+++ bychkov/0058-s-project/public/js/common.js
@@ -4,7 +4,65 @@
   svg4everybody({});
   // Custom JS
  
- 
+ // мобильное меню
+ var toggMnu = $(".toggle-mnu-1").click(function () {
+
+ 	$(".toggle-mnu-1").toggleClass("on");
+ 	// $("body").toggleClass("fixed");
+ 	$(".hidden-mnu").toggleClass("active");
+ 	$("body, html").toggleClass("fixed");
+ 	return false;
+ });
+
+   function heightses() {
+
+   	var w = $(window).width();
+   	// $(".otz__item .text-wrap ").height('auto').equalHeights();
+   	//
+
+
+   	// скрывает моб меню
+
+   	var topH = $("header ").innerHeight();
+
+   	$(window).scroll(function () {
+   		if ($(this).scrollTop() > topH) {
+   			$('.top-nav  ').addClass('fixed');
+   		} else {
+   			$('.top-nav  ').removeClass('fixed');
+   		}
+   	});
+   	// конец добавил
+	 }
+	 
+	   $(window).resize(function () {
+	   	heightses();
+
+	   });
+
+
+	   heightses();
+   if (window.matchMedia("(min-width: 992px)").matches) {
+
+   	$(".toggle-mnu-1").removeClass("on");
+   	// $("body").removeClass("fixed");
+   	$(".hidden-mnu").removeClass("active");
+   	$("body").removeClass("fixed");
+   }
+
+
+ $('.hidden-mnu ul li a').on('click', function () {
+ 	$(".hidden-mnu .toggle-mnu").click();
+ });
+ $(document).mouseup(function (e) {
+ 	var container = $(".hidden-mnu.active");
+ 	if (container.has(e.target).length === 0) {
+ 		$(".toggle-mnu-1").removeClass("on");
+ 		// $("body").toggleClass("fixed");
+ 		$(".hidden-mnu").removeClass("active");
+ 		$("body, html").removeClass("fixed");
+ 	}
+ });
 		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
 
 			type: 'iframe',
@@ -99,7 +157,15 @@
     mainClass: 'my-mfp-zoom-in'
   });
 
- 
+ // листалка по стр
+ $(" .top-nav a").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+
+            $('html, body').animate({ scrollTop: destination }, 1100);
+
+        return false;
+    });
  // маска на инпут
    $("input[type='tel']").attr("pattern","[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({"mask": "+7(999)999-99-99"});
 
