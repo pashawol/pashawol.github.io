0dd683c52cbc94af3b1c527ec6debcb7e206eade
Pavel <wol1414@gmail.com>
2018-09-05 14:44:56 +0300

    ыфвыв

--- bychkov/0053tapaz/public/js/common.js
+++ bychkov/0053tapaz/public/js/common.js
@@ -1,8 +1,7 @@
  jQuery(document).ready(function($) {
 
   // Custom JS
-
-  svg4everybody({});
+ 
 
     $(".s-main__btn-wrap-js").each(function() {
         var ch = $(this).children();
@@ -95,111 +94,7 @@
 
 	})
  heightses();
-
-// клавиатура
-// var kv =	$('#keyboard').keyboard({
-// 		// openOn: null,
-// 			stayOpen: true,
-// 				alwaysOpen: true,
-// 					layout: 'custom',
-// 						openOn: null,
-// 						stayOpen: true,
-// 						// 		// layout: 'qwerty'
-// 						display: {
-// 							'bksp': '\u2190',
-// 							'enter': 'return',
-// 							'normal': 'ABC',
-// 							'meta1': '.?123',
-// 							'meta2': '#+=',
-// 							'accept': '\u21d3'
-// 						},
-
-// 						layout: 'custom',
-// 						customLayout: {
-// 							'normal': [
-// 								'q w e r t y u i o p {bksp}',
-// 								'a s d f g h j k l {enter}',
-// 								'{s} z x c v b n m @ . {s}',
-// 								'{meta1} {space} _ - '
-// 							],
-// 							'shift': [
-// 								'Q W E R T Y U I O P {bksp}',
-// 								'A S D F G H J K L {enter}',
-// 								'{s} Z X C V B N M @ . {s}',
-// 								'{meta1} {space} _ - '
-// 							],
-// 							'meta1': [
-// 								'1 2 3 4 5 6 7 8 9 0 {bksp}',
-// 								'` | { } % ^ * / \' {enter}',
-// 								'{meta2} $ & ~ # = + . {meta2}',
-// 								'{normal} {space} ! ? '
-// 							],
-// 							'meta2': [
-// 								'[ ] { } \u2039 \u203a ^ * " , {bksp}',
-// 								'\\ | / &lt; &gt; $ \u00a3 \u00a5 \u2022 {enter}',
-// 								'{meta1} \u20ac & ~ # = + . {meta1}',
-// 								'{normal} {space} ! ? '
-// 							]
-// 						},
-
-
-// 						css: {
-// 							// input & preview
-					 
-// 							container: 'ui-widget-content ui-widget ui-corner-all ui-helper-clearfix ui-tel-mail',
-					
-					 
-// 						},
-// 			// layout: 'qwerty'
-// 	})
-	// // .addTyping()
-	// ;
-
-	// Typing Extension
-	// $('.btn-mail').click(function () {
-	//  kv;
-	// 	var kb = $('#keyboard').getkeyboard();
-	// 		if (kb.isOpen) {
-	// 			kb.close();
-	// 		} else {
-	// 			kb.reveal();
-	// 		}
-	// 		kb.reveal()
-	// // kb.reveal()
-	 
-	// });
-	
-	// $('.btn-tel').click(function () {
-	// var kb = 	$('#keyboard').getkeyboard();
-	// 	if (kb.isOpen) {
-	// 		kb.close();
-	// 	} else {
-	// 		kb.reveal();
-	// 	}
-	// kb.reveal()
-	// });
-
-
-
-
-	// $('#password')
-	// 	.keyboard({
-	// 		openOn: null,
-	// 		stayOpen: true,
-	// 		layout: 'qwerty'
-	// 	})
-	// 	// .addTyping()
-	// 	;
-
-	// $('#password-opener').click(function () {
-	// 	var kb = $('#password').getkeyboard();
-	// 	// close the keyboard if the keyboard is visible and the button is clicked a second time
-	// 	if (kb.isOpen) {
-	// 		kb.close();
-	// 	} else {
-	// 		kb.reveal();
-	// 	}
-	// });
+ 
 	
   $('#keyboard__mail').jkeyboard({
 		 layout: 'english__mail',
@@ -312,7 +207,7 @@
 	 });
 	 if( $("div").is(".gallery-top")){
 
-		 galleryTop.controller.control = galleryThumbs;
+		//  galleryTop.controller.control = galleryThumbs;
 		 galleryThumbs.controller.control = galleryTop;
 		}
 
