bcd7e6e72db99a16031f4e6b651f45f9a1627e55
Pavel <wol1414@gmail.com>
2018-09-29 01:50:57 +0300

    sadsdsa

--- AneOz/0002stador/public/js/common.js
+++ AneOz/0002stador/public/js/common.js
@@ -149,26 +149,7 @@ jQuery(document).ready(function ($) {
 			// instead of a settings object
 		]
 	});
-
-
-	// модальное окно
-	$('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
-
-		fixedContentPos: true,
-		fixedBgPos: true,
-
-		overflowY: 'auto',
-
-		closeBtnInside: true,
-		preloader: false,
-
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-zoom-in'
-	});
-
-
+ 
 
 	// маска на инпут
 	$("input[type='tel']").attr("pattern", "[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask({
