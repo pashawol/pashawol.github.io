cfeaac8b02e127167b229ce32540c84bb4959248
Pavel <wol1414@gmail.com>
2018-10-16 22:59:04 +0300

    ацаца

--- Ishmetov/0003baza/public/js/common.js
+++ Ishmetov/0003baza/public/js/common.js
@@ -3,24 +3,7 @@ jQuery(document).ready(function ($) {
 	// для свг
 	svg4everybody({});
 	// Custom JS
-
-	
-	var url = document.location.href;
-	$.each($(".top-nav__nav a "), function () {
-
-		if (this.href == url) {
-			if ($(this).hasClass("top-nav__link") == true) {
-
-				$(this).addClass('top-nav__link-active');
-			}
-			if ($(this).hasClass("footer__link") == true) {
-
-				$(this).addClass('footer__link-active');
-			}
-
-		};
-
-	});
+ 
 	// галерея
 	$(".gal").each(function () {
 
@@ -135,33 +118,7 @@ jQuery(document).ready(function ($) {
 	//        return false;
 	//    });
 
-
-	// табы  . Теперь данные активного таба остается в storage
-	$(function () {
-		var tab = ('tabs');
-
-		$('.' + tab + '__caption').each(function (i) {
-			var storage = localStorage.getItem('tab' + i);
-			if (storage) {
-				$(this).find('.' + tab + '__btn').removeClass('active').eq(storage).addClass('active')
-					.closest('.' + tab).find('.' + tab + '__content').removeClass('active').eq(storage).addClass('active');
-			}
-		});
-
-		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
-			$(this)
-				.addClass('active').siblings().removeClass('active')
-				.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-				.eq($(this).index()).fadeIn().addClass('active');
-
-			var ulIndex = $('.' + tab + '__caption').index($(this).parents('.' + tab + '__caption'));
-			localStorage.removeItem('tab' + ulIndex);
-			localStorage.setItem('tab' + ulIndex, $(this).index());
-
-		});
-	});
-
-
+ 
 
 	var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
 
@@ -228,5 +185,10 @@ jQuery(document).ready(function ($) {
 	//   });
 
   
-
+	$(".s-catalog__link--js").click(function (e) {
+		e.preventDefault();
+		var accord = $(this).next();
+		accord.hasClass("active") ?  (accord.slideUp().removeClass("active"), $(this).removeClass("active")) : (accord.slideDown().addClass("active"), $(this).addClass("active"));
+		 
+	  })
 });
