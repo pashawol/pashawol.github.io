a0889c3dcdd957693de23ea4e3c1fa89171b0405
pashawol <wol1414@gmail.com>
2018-10-19 18:10:48 +0300

    sqsqs

--- Ishmetov/0003baza/public/js/common.js
+++ Ishmetov/0003baza/public/js/common.js
@@ -4,6 +4,9 @@ jQuery(document).ready(function ($) {
 	svg4everybody({});
 	// Custom JS
  
+
+		var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
+
 	// галерея
 	$(".gal").each(function () {
 
@@ -16,7 +19,6 @@ jQuery(document).ready(function ($) {
 			 
 				cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
 			
-				titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
 				// Or the function that should return the title. For example:
 				// titleSrc: function(item) {
 				//   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
@@ -27,7 +29,9 @@ jQuery(document).ready(function ($) {
 				// tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
 			},
 			gallery: {
-				enabled: true
+				enabled: true,
+					arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%">' + icon + '</button>', // markup of an arrow button
+						titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
 			}
 		});
 	})
@@ -58,7 +62,7 @@ jQuery(document).ready(function ($) {
 
  
 
-	var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
+
 
 	var arrl2 = (' <div class="l">' + icon),
 		arrr2 = (' <div class="r">' + icon);
