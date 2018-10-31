5483241f3123ff4def2a7712a08faefc271105b9
Pavel <wol1414@gmail.com>
2018-11-01 00:08:36 +0300

    asdasd

--- ROONIX/0002Makita/public/js/common.js
+++ ROONIX/0002Makita/public/js/common.js
@@ -109,14 +109,14 @@ jQuery(document).ready(function ($) {
 
 
 	// листалка по стр
-	// $(" .top-nav a").click(function () {
-	//        var elementClick = $(this).attr("href");
-	//        var destination = $(elementClick).offset().top;
+	$(" .scroll-link").click(function () {
+	       var elementClick = $(this).attr("href");
+	       var destination = $(elementClick).offset().top;
 
-	//            $('html, body').animate({ scrollTop: destination }, 1100);
+	           $('html, body').animate({ scrollTop: destination }, 1100);
 
-	//        return false;
-	//    });
+	       return false;
+	   });
 
 
 	// табы  . 
@@ -371,7 +371,10 @@ image.parent().css('background-image', 'url(' + image.attr('src') + ')');
 
 	});
 	// /modal галерея
-
+$(".link-modal").fancybox({
+	arrows: false,
+	infobar: false,
+});
 
 	// mask for input
 	var customOptions = {
