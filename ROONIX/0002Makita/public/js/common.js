05587a309cfa1bddadb8db3cba63ad0968280a4e
Pavel <wol1414@gmail.com>
2018-11-05 11:23:53 +0300

    уцкцук

--- ROONIX/0002Makita/public/js/common.js
+++ ROONIX/0002Makita/public/js/common.js
@@ -120,11 +120,7 @@ jQuery(document).ready(function ($) {
 
 
 	// табы  . 
-	$(function () {
-		var tab = ('tabs');
-
-
-
+	function tabscostume(tab) { 
 		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
 			$(this)
 				.addClass('active').siblings().removeClass('active')
@@ -132,9 +128,10 @@ jQuery(document).ready(function ($) {
 				.eq($(this).index()).fadeIn().addClass('active');
 			$(".s-catalog__slider--js-2-prod").slick('reInit');
 		});
-	});
+	};
 
-
+	tabscostume('tab');
+	tabscostume('tabs-po');
 
 	var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
 
@@ -310,7 +307,7 @@ $('  .carusel')
 image.parent().css('background-image', 'url(' + image.attr('src') + ')');
 });
 
-	$(' .tabs__caption, .s-order__slider').slick({
+	$(' .tabs__caption, .s-order__slider, .tabs-po__caption').slick({
 		dots: false,
 		arrows: false,
 		infinite: false,
@@ -583,4 +580,7 @@ $(".link-modal").fancybox({
 
 	});
  
+	$(".table-history__tr--js").click(function(){
+		$(this).toggleClass("active").next().find('.table-history__inner-block--js').slideToggle(150);
+	})
 });
