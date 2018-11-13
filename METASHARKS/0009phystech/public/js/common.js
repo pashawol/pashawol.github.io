0d6b75bb2504be8a77cecf91a29845864537c8e5
Pavel <wol1414@gmail.com>
2018-11-13 16:08:06 +0300

    ВЫФЫВФЫВ

--- METASHARKS/0009phystech/public/js/common.js
+++ METASHARKS/0009phystech/public/js/common.js
@@ -314,30 +314,34 @@ var $li = $('.nav__item--has-children').hover(
 // анимация на главной вверху
 // MIT license
 // By @nodws with github.com/greensock/GreenSock-JS, see more examples at greensock.com/examples-showcases
-	(function() {
-		var lastTime = 0;
-		var vendors = ['ms', 'moz', 'webkit', 'o'];
-		for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-			window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-			window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-				|| window[vendors[x]+'CancelRequestAnimationFrame'];
-		}
+(function() {
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
+	for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+			|| window[vendors[x]+'CancelRequestAnimationFrame'];
+	}
 
-		if (!window.requestAnimationFrame)
-			window.requestAnimationFrame = function(callback, element) {
-				var currTime = new Date().getTime();
-				var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-				var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-					timeToCall);
-				lastTime = currTime + timeToCall;
-				return id;
-			};
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = function(callback, element) {
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+				timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
+		};
 
-		if (!window.cancelAnimationFrame)
-			window.cancelAnimationFrame = function(id) {
-				clearTimeout(id);
-			};
-	}());
+	if (!window.cancelAnimationFrame)
+		window.cancelAnimationFrame = function(id) {
+			clearTimeout(id);
+		};
+}());
+
+function anim1(){
+
+
 
 
 	(function() {
@@ -351,14 +355,14 @@ var $li = $('.nav__item--has-children').hover(
 
     function initHeader() {
 			
-			largeHeader = document.getElementById('large-header');
+			largeHeader = $('.large-header');
 			width = $(".animate-block").width();
 			height = $(".animate-block").height();
 			target = {x: width/2, y: height/2};
 //  
       // largeHeader.style.height = height+'px';
 
-        canvas = document.getElementById('x-canvas');
+        canvas = largeHeader.find('.x-canvas')[0];
         canvas.width = width;
         canvas.height = height;
         ctx = canvas.getContext('2d');
@@ -527,3 +531,335 @@ var $li = $('.nav__item--has-children').hover(
     }
 
 })();
+}
+// anim1();
+////////////////////////////////////////////////////////////////////
+
+
+// MIT license
+// By @nodws with github.com/greensock/GreenSock-JS, see more examples at greensock.com/examples-showcases
+ 
+
+function anim2(){
+	(function() {
+
+		var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+
+		// Main
+		initHeader();
+		initAnimation();
+		addListeners();
+
+		function initHeader() {
+				largeHeader = $('.large-header--blur');
+				width = $('.large-header--blur').width();
+				height = $('.large-header--blur').height(); 
+				target = {x: width/2, y: height/2};
+
+				largeHeader = $('.large-header--blur');
+
+			// largeHeader.style.height = height+'px';
+
+				canvas = largeHeader.find('.x-canvas--blur')[0];
+				canvas.width = width;
+				canvas.height = height;
+				ctx = canvas.getContext('2d');
+
+				// create points
+				points = [];
+				var puntitos=20;
+				for(var x = 0; x < width; x = x + width/puntitos) {
+						for(var y = 0; y < height; y = y + height/puntitos) {
+								var px = x + Math.random()*width/puntitos;
+								var py = y + Math.random()*height/puntitos;
+								var p = {x: px, originX: px, y: py, originY: py };
+								points.push(p);
+						}
+				}
+
+				// for each point find the 5 closest points
+				for(var i = 0; i < points.length; i++) {
+						var closest = [];
+						var p1 = points[i];
+						for(var j = 0; j < points.length; j++) {
+								var p2 = points[j]
+								if(!(p1 == p2)) {
+										var placed = false;
+										for(var k = 0; k < 5; k++) {
+												if(!placed) {
+														if(closest[k] == undefined) {
+																closest[k] = p2;
+																placed = true;
+														}
+												}
+										}
+
+										for(var k = 0; k < 5; k++) {
+												if(!placed) {
+														if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
+																closest[k] = p2;
+																placed = true;
+														}
+												}
+										}
+								}
+						}
+						p1.closest = closest;
+				}
+
+				// assign a circle to each point
+				for(var i in points) {
+						var c = new Circle(points[i], 2+Math.random()*2, 'rgba(200,200,255,255)');
+						points[i].circle = c;
+				}
+		}
+
+		// Event handling
+		function addListeners() {
+				if(!('ontouchstart' in largeHeader[0])) {
+						largeHeader[0].addEventListener('mousemove', mouseMove);
+				}
+				largeHeader[0].addEventListener('scroll', scrollCheck);
+				largeHeader[0].addEventListener('resize', resize);
+		}
+
+		function mouseMove(e) {
+				var posx = posy = 0;
+				if (e.pageX || e.pageY) {
+						posx = e.pageX;
+						posy = e.pageY;
+				}
+				else if (e.clientX || e.clientY)    {
+						posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+						posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+				}
+				target.x = posx;
+				target.y = posy;
+		}
+
+		function scrollCheck() {
+				if(document.body.scrollTop > height) animateHeader = false;
+				else animateHeader = true;
+		}
+
+		function resize() {
+				// largeHeader = $('.large-header--blur');
+				width = $('.large-header--blur').width();
+				height = $('.large-header--blur').height(); 
+				canvas.width = width;
+				canvas.height = height;
+		}
+
+		// animation
+		function initAnimation() {
+				animate();
+				for(var i in points) {
+						shiftPoint(points[i]);
+				}
+		}
+
+		function animate() {
+				if(animateHeader) {
+						ctx.clearRect(0,0,width,height);
+						for(var i in points) {
+								// detect points in range
+								if(Math.abs(getDistance(target, points[i])) < 4000) {
+										points[i].active = 0.4;
+										points[i].circle.active = 0.9;
+								} else if(Math.abs(getDistance(target, points[i])) < 60000) {
+										points[i].active = 0.3;
+										points[i].circle.active = 0.3;
+								} else if(Math.abs(getDistance(target, points[i])) < 100000) {
+										points[i].active = 0.1;
+										points[i].circle.active = 0.1;
+								} else {
+										points[i].active = 0.06;
+										points[i].circle.active = 0.05;
+								}
+
+								drawLines(points[i]);
+								points[i].circle.draw();
+						}
+				}
+				requestAnimationFrame(animate);
+		}
+
+		function shiftPoint(p) {
+				TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*50,
+						y: p.originY-50+Math.random()*50, ease:Circ.easeInOut,
+						onComplete: function() {
+								shiftPoint(p);
+						}});
+		}
+
+		// Canvas manipulation
+		function drawLines(p) {
+				if(!p.active) return;
+				for(var i in p.closest) {
+						ctx.beginPath();
+						ctx.moveTo(p.x, p.y);
+						ctx.lineTo(p.closest[i].x, p.closest[i].y);
+						ctx.strokeStyle = 'rgba(12,149,277,'+ p.active+')';
+						ctx.stroke();
+				}
+		}
+
+		function Circle(pos,rad,color) {
+				var _this = this;
+
+				// constructor
+				(function() {
+						_this.pos = pos || null;
+						_this.radius = rad || null;
+						_this.color = color || null;
+				})();
+
+				this.draw = function() {
+						if(!_this.active) return;
+						ctx.beginPath();
+						ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+						ctx.fillStyle = 'rgba(255,255,255,'+ _this.active+')';
+						ctx.fill();
+				};
+		}
+
+		// Util
+		function getDistance(p1, p2) {
+				return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+		}
+
+	})();
+
+}
+// anim2();
+
+// var canvasDiv = $('#particle-canvas')[0];
+// var options = {
+//   particleColor: '#0c95ed',
+// 	background: '#fff',
+//   interactive: true,
+//   speed: 'fast',
+//   density: 'high'
+// };
+// var particleCanvas = new ParticleNetwork(canvasDiv, options);
+
+
+
+/* ---- particles.js config ---- */
+
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 900
+      }
+    },
+    "color": {
+      "value": "#a3cde8"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 2
+			}
+			// ,
+      // "image": {
+      //   "src": "img/github.svg",
+      //   "width": 1000,
+      //   "height": 1000
+      // }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#0c95ed",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 240,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 100,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 600,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+
+ 
