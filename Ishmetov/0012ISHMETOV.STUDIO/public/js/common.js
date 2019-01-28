a985c986379cee8d851b8fed6aa8ab1d05053ab1
Pavel <wol1414@gmail.com>
2019-01-28 22:42:49 +0300

    тлл

--- Ishmetov/0012ISHMETOV.STUDIO/public/js/common.js
+++ Ishmetov/0012ISHMETOV.STUDIO/public/js/common.js
@@ -59,25 +59,7 @@ jQuery(document).ready(function ($) {
 
 	function heightses() {
 
-		var w = $(window).width();
-
-		// $(".main-wrapper").css("margin-bottom", $('footer').height())
-		// $(".otz__item .text-wrap ").height('auto').equalHeights();
-		//
-
-
-		// скрывает моб меню
-
-		// var topH = $("header ").innerHeight();
-
-		// $(window).scroll(function () {
-		// 	if ($(this).scrollTop() > topH) {
-		// 		$('.top-nav  ').addClass('fixed');
-		// 	} else {
-		// 		$('.top-nav  ').removeClass('fixed');
-		// 	}
-		// });
-		// // конец добавил
+		var w = $(window).width(); 
 	}
 
 	if (window.matchMedia("(min-width: 992px)").matches) {
@@ -98,83 +80,22 @@ jQuery(document).ready(function ($) {
 
 	})
 
-	heightses();
-
-
-
-	// листалка по стр
-	// $(" .top-nav a").click(function () {
-	//        var elementClick = $(this).attr("href");
-	//        var destination = $(elementClick).offset().top;
-
-	//            $('html, body').animate({ scrollTop: destination }, 1100);
-
-	//        return false;
-	//    });
-
-
-	// табы  . 
-	function tabscostume(tab) {
-		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
-			$(this)
-				.addClass('active').siblings().removeClass('active')
-				.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-				.eq($(this).index()).fadeIn().addClass('active');
-
-		});
-	};
-	tabscostume('tabs');
-
-
-
-	var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
-
-	var arrl2 = (' <div class="r">' + icon),
-		arrr2 = (' <div class="l">' + icon);
-	// карусель
-	$('.s-team__slider').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		dots: false,
-		speed: 450,
-		infinite: true,
-		loop: true,
-		arrows: true,
-		mobileFirst: true,
-		// centerMode: true,
-		// focusOnSelect: true ,
-		// variableWidth: true,
-		prevArrow: arrr2,
-		nextArrow: arrl2,
-	});
-
-	
+	heightses();  
+ 
 	$('.main-block__slider--js').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		dots: false,
-		speed: 600,
+		speed: 1200,
 		infinite: true,
 		loop: true,
 		// verticalSwiping	: true,
 		arrows: false,
 		autoplay: true,
-		autoplaySpeed: 900,
-		mobileFirst: true,
-		responsive: [{
-			
-			breakpoint: 768,
-      settings: {
-				vertical: true, 
-      }
-
-    } ]
-		// adaptiveHeight: true,
-		// centerMode: true,
-		// focusOnSelect: true ,
-		// variableWidth: true,
-		// prevArrow: arrr2,
-		// nextArrow: arrl2,
+		autoplaySpeed: 1800,
+		// mobileFirst: true,
+		vertical: true, 
+ 
 	});
 
 
