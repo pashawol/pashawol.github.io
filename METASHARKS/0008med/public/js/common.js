15e6d679d8bd4fcac1e10d61f6ed9233ecec9dc0
Pavel Khalunin <wol1414@gmail.com>
2019-03-04 16:08:31 +0300

    add git

--- METASHARKS/0008med/public/js/common.js
+++ METASHARKS/0008med/public/js/common.js
@@ -1,3 +1,111 @@
+
+// Для лэзи загрузки
+function lazyLoad(){
+
+	
+	document.addEventListener("DOMContentLoaded", function() {
+		let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+		let active = false;
+		
+		const lazyLoad = function() {
+			if (active === false) {
+				active = true;
+				
+				setTimeout(function() {
+					lazyImages.forEach(function(lazyImage) {
+						if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+							lazyImage.src = lazyImage.dataset.src;
+							// lazyImage.srcset = lazyImage.dataset.srcset;
+							lazyImage.classList.remove("lazy");
+							
+							lazyImages = lazyImages.filter(function(image) {
+								return image !== lazyImage;
+							});
+							
+							if (lazyImages.length === 0) {
+								document.removeEventListener("scroll", lazyLoad);
+								window.removeEventListener("resize", lazyLoad);
+								window.removeEventListener("orientationchange", lazyLoad);
+								window.addEventListener("DOMContentLoaded", lazyLoad);
+							}
+						}
+					});
+					
+					active = false;
+				}, 200);
+			}
+		};
+		
+		document.addEventListener("scroll", lazyLoad);
+		window.addEventListener("resize", lazyLoad);
+		window.addEventListener("orientationchange", lazyLoad);
+		window.addEventListener("DOMContentLoaded", lazyLoad);
+	});
+	
+	
+	// лэзи 
+	document.addEventListener("DOMContentLoaded", function() {
+		let lazyImages = [].slice.call(document.querySelectorAll(".lazy-bg"));
+		let active = false;
+		
+		const lazyLoad = function() {
+			if (active === false) {
+				active = true;
+				
+				setTimeout(function() {
+					lazyImages.forEach(function(lazyImage) {
+						if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+							lazyImage.parentElement.style.backgroundImage = 'url(' + lazyImage.dataset.src +')';
+							lazyImage.src = lazyImage.dataset.src;
+							// lazyImage.srcset = lazyImage.dataset.srcset;
+							lazyImage.classList.remove("lazy");
+							
+							lazyImages = lazyImages.filter(function(image) {
+								return image !== lazyImage;
+							});
+							
+							if (lazyImages.length === 0) {
+								document.removeEventListener("scroll", lazyLoad);
+								window.removeEventListener("resize", lazyLoad);
+								window.removeEventListener("orientationchange", lazyLoad);
+								window.addEventListener("DOMContentLoaded", lazyLoad);
+							}
+						}
+					});
+					
+					active = false;
+				}, 200);
+			}
+		};
+		
+		document.addEventListener("scroll", lazyLoad);
+		window.addEventListener("resize", lazyLoad);
+		window.addEventListener("orientationchange", lazyLoad);
+		window.addEventListener("DOMContentLoaded", lazyLoad);
+	});
+	
+	
+	
+	document.addEventListener("DOMContentLoaded", function() {
+		var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
+		
+		if ("IntersectionObserver" in window) {
+			let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+				entries.forEach(function(entry) {
+					if (entry.isIntersecting) {
+						entry.target.classList.add("visible");
+						lazyBackgroundObserver.unobserve(entry.target);
+					}
+				});
+			});
+			
+			lazyBackgrounds.forEach(function(lazyBackground) {
+				lazyBackgroundObserver.observe(lazyBackground);
+			});
+		}
+	});
+}
+lazyLoad();
 jQuery(document).ready(function ($) {
 
 	// для свг
@@ -199,6 +307,7 @@ jQuery(document).ready(function ($) {
 		// autoplaySpeed: 3000, 
 		prevArrow: arrl2,
 		nextArrow: arrr2,
+		
 		responsive: [
 
 			{
@@ -232,28 +341,38 @@ jQuery(document).ready(function ($) {
 	});
 
 	$('.s-catalog__slider').slick({
-		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToShow: 1,
+		slidesToScroll: 1,
 		dots: false,
 		speed: 450,
 		infinite: true,
 		loop: true,
 		arrows: true,
+		mobileFirst: true,
 		//  autoplay: true,
 		// autoplaySpeed: 3000, 
 		prevArrow: arrl2,
 		nextArrow: arrr2,
 		responsive: [{
-				breakpoint: 1199,
+				breakpoint: 1200,
+				settings: {
+
+					slidesToShow: 4,
+					slidesToScroll: 4,
+				}
+			},
+
+			{
+				breakpoint: 992,
 				settings: {
 
 					slidesToShow: 3,
 					slidesToScroll: 3,
 				}
 			},
-
+			
 			{
-				breakpoint: 991,
+				breakpoint: 576,
 				settings: {
 
 					slidesToShow: 2,
